3430c808b6706dee4684b1fb1bc2a222048e9976
meows <b5c6@protonmail.com>
2019-11-13 14:17:45 -0500

    core: set mordor genesis block timestamp

--- core/genesis_mordor.go
+++ core/genesis_mordor.go
@@ -27,6 +27,7 @@ func DefaultMordorGenesisBlock() *Genesis {
 		ExtraData:  hexutil.MustDecode("0x70686f656e697820636869636b656e206162737572642062616e616e61"),
 		GasLimit:   hexutil.MustDecodeUint64("0x2fefd8"),
 		Difficulty: hexutil.MustDecodeBig("0x20000"),
+		Timestamp: hexutil.MustDecodeUint64("0x5d9676db"),
 		Alloc:      GenesisAlloc{},
 	}
 }

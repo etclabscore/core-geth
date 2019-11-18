9c31b58f717ba6a00fc70227de2328ac24e5db87
meows <b5c6@protonmail.com>
2019-11-18 05:25:21 -0500

    chainspecs: script and sync parity chainspecs jsons

---
+++ chainspecs/sync-parity-chainspecs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+specs=(
+	"frontier_test"
+	"homestead_test"
+	"eip150_test"
+	"eip161_test"
+	"eip210_test"
+	"byzantium_test"
+	"constantinople_test"
+	"st_peters_test"
+	"istanbul_test"
+	"transition_test"
+
+	"foundation"
+	"goerli"
+	"rinkeby"
+	"ropsten"
+
+	"classic"
+	"morden"
+	"mordor"
+	"kotti"
+
+	"mix"
+	"musicoin"
+)
+
+if [[ $# -ne 0 ]]; then
+	specs=("$*")
+fi
+
+for spec_name in "${specs[@]}"; do
+	curl -O https://raw.githubusercontent.com/paritytech/parity-ethereum/master/ethcore/res/ethereum/"$spec_name".json
+done

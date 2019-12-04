5ab01929b2e1ca370c761881a9061b6efbe13d05
ia <b5c6@protonmail.com>
2019-12-04 13:35:47 -0500

    Revert "uncommit me"
    This reverts commit 4d160a2d740f87594477fcfa3dd30e316ac8d947.

--- params/types/gen_genesis.go
+++ params/types/gen_genesis.go
@@ -28,6 +28,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
 	common0 "github.com/ethereum/go-ethereum/params/types/common"
+	"github.com/ethereum/go-ethereum/params/types/goethereum"
 )
 
 var _ = (*genesisSpecMarshaling)(nil)
@@ -86,9 +87,20 @@ func (g *Genesis) UnmarshalJSON(input []byte) error {
 		ParentHash *common.Hash                                `json:"parentHash"`
 	}
 	var dec Genesis
-	if err := json.Unmarshal(input, &dec); err != nil {
-		return err
+
+	// Note that this logic is importantly relate to the logic in params/convert/json.go, for ChainConfigurator
+	// unmarshaling.
+	dec.Config = &MultiGethChainConfig{}
+	if err := json.Unmarshal(input, &dec); err != nil || common0.IsValid(dec.Config, nil) != nil {
+		dec.Config = &goethereum.ChainConfig{}
+		if err := json.Unmarshal(input, &dec); err != nil {
+			return err
+		}
+		if err := common0.IsValid(dec.Config, nil); err !=nil {
+			return err
+		}
 	}
+
 	if dec.Config != nil {
 		g.Config = dec.Config
 	}

e69647b87a4e29cbdc2fcfb40c361f3877b14bdf
meows <b5c6@protonmail.com>
2019-12-04 12:24:00 -0500

    params/types/parity: set associated DAO fork data when setting transition
    This transition is VERY SPECIFIC to THIS FORK.
    Associated data to the transition value should not be left empty.

--- params/convert/generic.go
+++ params/convert/generic.go
@@ -1,9 +1,13 @@
 package convert
 
 import (
+	"fmt"
+
+	paramtypes "github.com/ethereum/go-ethereum/params/types"
 	"github.com/ethereum/go-ethereum/params/types/common"
 	"github.com/ethereum/go-ethereum/params/types/goethereum"
 	"github.com/ethereum/go-ethereum/params/types/parity"
+	"github.com/ethereum/go-ethereum/params/vars"
 )
 
 // GenericCC is a generic-y struct type used to expose some meta-logic methods
@@ -23,8 +27,14 @@ func (c GenericCC) DAOSupport() bool {
 	if gc, ok := c.ChainConfigurator.(*goethereum.ChainConfig); ok {
 		return gc.DAOForkSupport
 	}
-	if pc, ok := c.ChainConfigurator.(*parity.ParityChainSpec); ok {
-		return pc.Engine.Ethash.Params.DaoHardforkBeneficiary != nil
+	if mg, ok := c.ChainConfigurator.(*paramtypes.MultiGethChainConfig); ok {
+		return mg.GetEthashEIP779Transition() != nil
 	}
-	return c.GetEthashEIP779Transition() != nil
+	if pc, ok := c.ChainConfigurator.(*parity.ParityChainSpec); ok {
+		return pc.Engine.Ethash.Params.DaoHardforkTransition != nil &&
+			pc.Engine.Ethash.Params.DaoHardforkBeneficiary != nil &&
+			*pc.Engine.Ethash.Params.DaoHardforkBeneficiary == vars.DAORefundContract &&
+			len(pc.Engine.Ethash.Params.DaoHardforkAccounts) == len(vars.DAODrainList())
+	}
+	panic(fmt.Sprintf("uimplemented DAO logic, config: %v", c.ChainConfigurator))
 }

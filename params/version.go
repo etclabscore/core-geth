8f4b1ddc8458e76980a73bb07bb82586b386f98e
meows <b5c6@protonmail.com>
2019-12-16 07:50:25 -0600

    params: bumb to stable for release 1.9.5
    Signed-off-by: meows <b5c6@protonmail.com>

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1          // Major version component of the current release
 	VersionMinor = 9          // Minor version component of the current release
 	VersionPatch = 5          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 	VersionName  = "MultiGeth"
 )
 

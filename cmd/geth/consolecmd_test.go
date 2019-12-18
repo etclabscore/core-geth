f6ddb60071fdd3941cba034f138b134b4e03ed0b
meows <b5c6@protonmail.com>
2019-12-18 12:23:59 -0600

    cmd/geth: add test for chain identities variables at runtime
    flags.go is a complicated file. It doesn't have great testing.
    It largely determines how geth behaves given user parameters.
    This test intendes to shore up expectations for consensus-impacting
    chain variables given their configuration via CLI flag parameters
    for a geth runtime instance.
    
    This is important because I caught network ids misbehaving
    despite unit tests all passing because the value was only wrong
    at runtime.
    
    The importance of decent integration tests for large and complex
    systems like geth are hard to understate.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/geth/consolecmd_test.go
+++ cmd/geth/consolecmd_test.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
 	"os"
 	"path/filepath"
@@ -34,6 +35,54 @@ const (
 	ipcAPIs  = "admin:1.0 debug:1.0 eth:1.0 ethash:1.0 miner:1.0 net:1.0 personal:1.0 rpc:1.0 shh:1.0 txpool:1.0 web3:1.0"
 	httpAPIs = "eth:1.0 net:1.0 rpc:1.0 web3:1.0"
 )
+
+// TestConsoleCmdNetworkIdentities tests network identity variables at runtime for a geth instance.
+// This provides a "production equivalent" integration test for consensus-relevant chain identity values which
+// cannot be adequately unit tested because of reliance on cli context variables.
+// These tests should cover expected default values and possible flag-interacting values, like --<chain> with --networkid=n.
+func TestConsoleCmdNetworkIdentities(t *testing.T) {
+	chainIdentityCases := []struct {
+		flags      []string
+		networkId int
+		chainId int
+		genesisHash string
+	}{
+		// Default chain value, without and with --networkid flag set.
+		{[]string{}, 1, 1, params.MainnetGenesisHash.Hex()},
+		{[]string{"--networkid", "42"}, 42, 1, params.MainnetGenesisHash.Hex()},
+
+		// Non-default chain value, without and with --networkid flag set.
+		{[]string{"--classic"}, 1, 61, params.MainnetGenesisHash.Hex()},
+		{[]string{"--classic", "--networkid", "42"}, 42, 61, params.MainnetGenesisHash.Hex()},
+
+		// All other possible --<chain> values.
+		{[]string{"--testnet"}, 3, 3, params.TestnetGenesisHash.Hex()},
+		{[]string{"--rinkeby"}, 4, 4, params.RinkebyGenesisHash.Hex()},
+		{[]string{"--goerli"}, 5, 5, params.GoerliGenesisHash.Hex()},
+		{[]string{"--kotti"}, 6, 6, params.KottiGenesisHash.Hex()},
+		{[]string{"--mordor"}, 7, 63, params.MordorGenesisHash.Hex()},
+		{[]string{"--social"}, 28, 28, params.SocialGenesisHash.Hex()},
+		{[]string{"--ethersocial"}, 1, 31102, params.EthersocialGenesisHash.Hex()},
+	}
+	for i, p := range chainIdentityCases {
+		t.Run(fmt.Sprintf("%d/%v/networkid", i, p.flags),
+			consoleCmdStdoutTest(p.flags, "admin.nodeInfo.protocols.eth.network", p.networkId))
+		t.Run(fmt.Sprintf("%d/%v/chainid", i, p.flags),
+			consoleCmdStdoutTest(p.flags, "admin.nodeInfo.protocols.eth.config.chainId", p.chainId))
+		t.Run(fmt.Sprintf("%d/%v/genesis_hash", i, p.flags),
+			consoleCmdStdoutTest(p.flags, "eth.getBlock(0, false).hash", strconv.Quote(p.genesisHash)))
+	}
+}
+
+func consoleCmdStdoutTest(flags []string, execCmd string, want interface{}) func(t *testing.T) {
+	return func(t *testing.T) {
+		flags = append(flags, "--exec", execCmd, "console")
+		geth := runGeth(t, flags...)
+		geth.Expect(fmt.Sprintf(`%v
+`, want))
+		geth.ExpectExit()
+	}
+}
 
 // Tests that a node embedded within a console can be started up properly and
 // then terminated by closing the input stream.

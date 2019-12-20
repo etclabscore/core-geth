bf83e0891b237ed5c879411b9e028af6b1f671dd
meows <b5c6@protonmail.com>
2019-12-19 18:23:42 -0600

    core/vm: fix method+sig for new oog contracts test
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -421,7 +421,7 @@ func testPrecompiled(addr string, test precompiledTest, t *testing.T) {
 }
 
 func testPrecompiledOOG(addr string, test precompiledTest, t *testing.T) {
-	p := PrecompiledContractsIstanbul[common.HexToAddress(addr)]
+	p := PrecompiledContractsForConfig(params.AllEthashProtocolChanges, big.NewInt(0))[common.HexToAddress(addr)]
 	in := common.Hex2Bytes(test.input)
 	contract := NewContract(AccountRef(common.HexToAddress("1337")),
 		nil, new(big.Int), p.RequiredGas(in)-1)

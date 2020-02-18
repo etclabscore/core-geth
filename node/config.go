71a149eaf19b5f2524ab4cc0abfc6dbd92892bd7
Talha Cross <47772477+soc1c@users.noreply.github.com>
2020-02-18 15:20:47 +0100

    Update config.go

--- node/config.go
+++ node/config.go
@@ -289,7 +289,7 @@ func (c *Config) NodeName() string {
 	name := c.name()
 	// Backwards compatibility: previous versions used Geth or MultiGeth
 	if strings.ToLower(name) == "geth" || strings.ToLower(name) == "geth-testnet" || strings.ToLower(name) == "multigeth" {
-		name = "PhoenixGeth"
+		name = "CoreGeth"
 	}
 	if params.VersionName != "" {
 		name = params.VersionName

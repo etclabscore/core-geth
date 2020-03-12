70606a31ac6756cb570557aab192c7be9eefb68b
meows <b5c6@protonmail.com>
2020-03-12 11:20:31 -0500

    : include script for developing openrpc document
    This is just a quick little developer tool.
    It'll produce some logs which we can include in the
    development branch as well. I hope this'll help with
    transparency of workflow and easier review.
    
    Signed-off-by: meows <b5c6@protonmail.com>

---
+++ develop-openrpc-doc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+[[ -f openrpc.json ]] || { wget --no-check-certificate https://raw.githubusercontent.com/etclabscore/ethereum-json-rpc-specification/master/openrpc.json; }
+
+if ! pgrep geth > /dev/null 2>&1; then
+(
+make geth && ./build/bin/geth --datadir=/tmp/gethddd --nodiscover --maxpeers=0 --rpc --rpcapi=admin,debug,eth,ethash,miner,net,personal,rpc,txpool,web3
+)&
+fi
+
+# Logs are truncated with each run, they only show the latest state.
+http --json POST http://localhost:8545 id:=$(date +%s) method=rpc_setOpenRPCDiscoverDocument params:='["./openrpc.json"]' > openrpc_set.log
+http --json POST http://localhost:8545 id:=$(date +%s) method=rpc_describeOpenRPC params:='[]' > openrpc_describe.log
+
+# Developer can then inspect the logs, eg.
+
+# cat openrpc_describe.log | jj -p | head -40
+

22f93fed0947140da2ba37e7c3d6a05f2c505c12
meows <b5c6@protonmail.com>
2020-03-27 09:05:40 -0500

    .develop/test.sh: fixup test script
    Signed-off-by: meows <b5c6@protonmail.com>

--- .develop/test.sh
+++ .develop/test.sh
@@ -18,19 +18,19 @@ disown
 echo "Geth PID: ${gethpid}"
 
 # Wait for geth to start up.
-echo "Waiting 3for geth to startup..."
+echo "Waiting 3 for geth to startup..."
 sleep 3
 
 # Save a copy of the generated openrpc document by HTTP RPC query.
 http --json POST http://localhost:8545 id:=$(date +%s) method='rpc_describe' params:='[]' | jj -p 'result' | tee "$(pwd)/.develop/spec.json"
 
+# Update our gist before validation; the script will exit if the validator fails.
+gist -u 4da4c08765679dac1899543002d1f545 "$(pwd)/.develop/spec.json" >/dev/null 2>&1 &
+
 # Run the OpenRPC document validator.
 # Not sure the install here actually works. FIXME.
 # command -v openrpc-generator-cli >/dev/null 2>&1 || { npm install -g @etclabscore/openrpc-generator-cli; }
 openrpc-validator-cli "$(pwd)/.develop/spec.json"
-
-# Update our gist before validation; the script will exit if the validator fails.
-gist -u 4da4c08765679dac1899543002d1f545 "$(pwd)/.develop/spec.json" &
 
 kill $gethpid
 

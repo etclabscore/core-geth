8354c39c84445d266a786a87b35da10d9855cc13
meows <b5c6@protonmail.com>
2020-04-15 10:48:12 -0500

    build/ssvm.sh: bump SSVM to 0.5.0
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/ssvm.sh
+++ build/ssvm.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-mkdir -p build/_workspace
-[ ! -d build/_workspace/SSVM ] && git clone https://github.com/second-state/SSVM build/_workspace/SSVM || echo "SSVM exists."
-cd build/_workspace/SSVM
-git fetch --tags
-# git checkout 0.4.0
-git checkout 8631d4606531981524b347ebb72291cd0b343ab0
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON ..
-make
-echo "Built library at: $(pwd)/tools/ssvm-evmc/libssvmEVMC.so"
+mkdir -p build/_workspace/SSVM/build/tools/ssvm-evmc/
+wget --no-check-certificate -O build/_workspace/SSVM/build/tools/ssvm-evmc/libssvmEVMC.so https://github.com/second-state/SSVM/releases/download/0.5.0/libssvmEVMC-linux-x86_64.so
+
+#mkdir -p build/_workspace
+#[ ! -d build/_workspace/SSVM ] && git clone https://github.com/second-state/SSVM build/_workspace/SSVM || echo "SSVM exists."
+#cd build/_workspace/SSVM
+#git fetch --tags
+## git checkout 0.5.0
+#git checkout 0.5.0
+#mkdir -p build
+#cd build
+#cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON ..
+#make
+#echo "Built library at: $(pwd)/tools/ssvm-evmc/libssvmEVMC.so"

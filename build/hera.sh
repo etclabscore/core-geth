c67892a6a08168e9c8a8d17e203c96cfd96fb156
meows <b5c6@protonmail.com>
2020-05-04 19:36:25 -0500

    .github/workflows/evmc.yml,.travis.yml,build/hera.sh: use Github Actions for EVMC SO tests intead of travis
    Travis is a pain in the ass.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/hera.sh
+++ build/hera.sh
@@ -2,19 +2,12 @@
 
 set -e
 
-mkdir -p build/_workspace/hera/build/src
-wget --no-check-certificate -O build/_workspace/hera/hera-0.2.5-linux-x86_64.tar.gz https://github.com/ewasm/hera/releases/download/v0.2.5/hera-0.2.5-linux-x86_64.tar.gz
-mkdir build/_workspace/hera/tmp
-tar -C build/_workspace/hera/tmp -zxvf build/_workspace/hera/hera-0.2.5-linux-x86_64.tar.gz
-mv build/_workspace/hera/tmp/lib/libhera.so build/_workspace/hera/build/src/libhera.so
-rm -rf build/_workspace/hera/tmp
-
-#mkdir -p build/_workspace
-#[ ! -d build/_workspace/hera ] && git clone https://github.com/ewasm/hera build/_workspace/hera || echo "Hera exists."
-#cd build/_workspace/hera
-#git submodule update --init
-#mkdir -p build
-#cd build
-#cmake -DBUILD_SHARED_LIBS=ON ..
-#cmake --build .
-#echo "Built library at: $(pwd)/src/libhera.so"
+mkdir -p build/_workspace
+[ ! -d build/_workspace/hera ] && git clone https://github.com/ewasm/hera build/_workspace/hera || echo "Hera exists."
+cd build/_workspace/hera
+git submodule update --init
+mkdir -p build
+cd build
+cmake -DBUILD_SHARED_LIBS=ON ..
+cmake --build .
+echo "Built library at: $(pwd)/src/libhera.so"

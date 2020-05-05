0b70a86cd6e978b5df8a16048146fe9a0a3b29b4
meows <b5c6@protonmail.com>
2020-05-05 10:54:37 -0500

    core: remove unused import
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/state_transition.go
+++ core/state_transition.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params/vars"
 )
 

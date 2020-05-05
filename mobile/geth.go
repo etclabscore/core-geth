6d2b698ac3b78a58d0089d8e582b764c962ab46d
meows <b5c6@protonmail.com>
2020-05-05 11:05:57 -0500

    geth: fix Testnet -> Ropsten
    Signed-off-by: meows <b5c6@protonmail.com>

--- mobile/geth.go
+++ mobile/geth.go
@@ -148,8 +148,8 @@ func NewNode(datadir string, config *NodeConfig) (stack *Node, _ error) {
 			return nil, fmt.Errorf("invalid genesis spec: %v", err)
 		}
 		// If we have the testnet, hard code the chain configs too
-		if config.EthereumGenesis == TestnetGenesis() {
-			genesis.Config = params.TestnetChainConfig
+		if config.EthereumGenesis == RopstenGenesis() {
+			genesis.Config = params.RopstenChainConfig
 			if config.EthereumNetworkID == 1 {
 				config.EthereumNetworkID = 3
 			}

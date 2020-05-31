b1a04560d174fae3cc17bd674c378336c4c28826
meows <b5c6@protonmail.com>
2020-05-31 11:05:41 -0500

    core,params: fixes DAO fork state activation for supporting configs
    This was misconfigured, causing ETH chain config to
    fail to syncronize with the Foundation mainnet.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/state_processor.go
+++ core/state_processor.go
@@ -62,8 +62,11 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		gp       = new(GasPool).AddGas(block.GasLimit())
 	)
 	// Mutate the block and state according to any hard-fork specs
-	if p.config.IsEnabled(p.config.GetEthashEIP779Transition, block.Number()) {
-		misc.ApplyDAOHardFork(statedb)
+	isDAOSupport := p.config.IsEnabled(p.config.GetEthashEIP779Transition, block.Number())
+	if isDAOSupport {
+		if daoNumber := p.config.GetEthashEIP779Transition(); daoNumber != nil && *daoNumber == block.NumberU64() {
+			misc.ApplyDAOHardFork(statedb)
+		}
 	}
 	// Iterate over and process the individual transactions
 	for i, tx := range block.Transactions() {

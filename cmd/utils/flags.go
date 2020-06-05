18ee31bff434d9b62074f37533a73f8e0b213c79
meows <b5c6@protonmail.com>
2020-06-05 11:39:12 -0500

    utils: (noop) remove done TODO
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1552,7 +1552,6 @@ func SetShhConfig(ctx *cli.Context, stack *node.Node, cfg *whisper.Config) {
 // SetEthConfig applies eth-related command line flags to the config.
 func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *eth.Config) {
 	// Avoid conflicting network flags
-	// TODO(meowsbits)
 	CheckExclusive(ctx, DeveloperFlag, LegacyTestnetFlag, RopstenFlag, RinkebyFlag, GoerliFlag, YoloV1Flag, ClassicFlag, KottiFlag, MordorFlag, EthersocialFlag, SocialFlag)
 	CheckExclusive(ctx, LegacyLightServFlag, LightServeFlag, SyncModeFlag, "light")
 	CheckExclusive(ctx, DeveloperFlag, ExternalSignerFlag) // Can't use both ephemeral unlocked and external signer

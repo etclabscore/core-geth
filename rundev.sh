95609d9f50e4f8ba7068762d7378382d871a6da5
meows <b5c6@protonmail.com>
2020-06-10 14:04:38 -0500

    rundev.sh,runtest.sh: dev-period, timeout++
    Signed-off-by: meows <b5c6@protonmail.com>

--- rundev.sh
+++ rundev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-make geth
-./build/bin/geth --dev --miner.etherbase f6d7e4e39f35a0b8f61dd0a24a2dc92a3a5e0b01 --ipcpath /tmp/geth_dev.ipc --keystore $(pwd)/.ia/ks --miner.gaslimit=8000000 --verbosity 5 --http --http.corsdomain='*' console 2> geth.log
+make getj
+./build/bin/geth --dev --dev.period 1 --miner.etherbase f6d7e4e39f35a0b8f61dd0a24a2dc92a3a5e0b01 --ipcpath /tmp/geth_dev.ipc --keystore $(pwd)/.ia/ks --miner.gaslimit=8000000 --verbosity 5 --http --http.corsdomain='*' console 2> geth.log
 #./build/bin/geth --dev --dev.period 5 --ipcpath /tmp/geth_dev.ipc --keystore ./ks --miner.gaslimit=8000000 --verbosity 5 console 2> geth.log

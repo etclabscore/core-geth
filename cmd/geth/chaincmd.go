cf17e5626f9682a8d085db3b2c1e50d281ffb5eb
meows <b5c6@protonmail.com>
2020-06-18 14:55:09 -0500

    main: exit with status code 1 if import returns error
    The import command should not return a 0 status
    code if the import finishes prematurely becaues
    of an import error.
    
    Returning the error causes the program to exit with 1
    if the err is non nil.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/geth/chaincmd.go
+++ cmd/geth/chaincmd.go
@@ -315,13 +315,17 @@ func importChain(ctx *cli.Context) error {
 	// Import the chain
 	start := time.Now()
 
+	var importErr error
+
 	if len(ctx.Args()) == 1 {
 		if err := utils.ImportChain(chain, ctx.Args().First()); err != nil {
+			importErr = err
 			log.Error("Import error", "err", err)
 		}
 	} else {
 		for _, arg := range ctx.Args() {
 			if err := utils.ImportChain(chain, arg); err != nil {
+				importErr = err
 				log.Error("Import error", "file", arg, "err", err)
 			}
 		}
@@ -374,7 +378,7 @@ func importChain(ctx *cli.Context) error {
 		utils.Fatalf("Failed to read database iostats: %v", err)
 	}
 	fmt.Println(ioStats)
-	return nil
+	return importErr
 }
 
 func exportChain(ctx *cli.Context) error {

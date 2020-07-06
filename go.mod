5de4c880267b9dcbd06171b000123ecbc9641a4c
meows <b5c6@protonmail.com>
2020-07-06 14:57:21 -0500

    go.mod,go.sum,node: initial addition of openrpc log AND DEPENDENCIES
    Dependencies added and lights green, notably
    using meowsbits/meta-schema @ v0.0.43 instead of
    open-rpc/meta-schema @ latest. Upstream at master
    is not working for me.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module github.com/ethereum/go-ethereum
 go 1.13
 
 require (
-	github.com/Azure/azure-pipeline-go v0.2.2 // indirect
 	github.com/Azure/azure-storage-blob-go v0.7.0
-	github.com/Azure/go-autorest/autorest/adal v0.8.0 // indirect
-	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/VictoriaMetrics/fastcache v1.5.7
+	github.com/alecthomas/jsonschema v0.0.0-20200530073317-71f438968921
 	github.com/aristanetworks/goarista v0.0.0-20170210015632-ea17b1a17847
 	github.com/aws/aws-sdk-go v1.25.48
 	github.com/btcsuite/btcd v0.0.0-20171128150713-2e60448ffcc6
@@ -15,20 +13,17 @@ require (
 	github.com/cloudflare/cloudflare-go v0.10.2-0.20190916151808-a80f83b9add9
 	github.com/davecgh/go-spew v1.1.1
 	github.com/deckarep/golang-set v0.0.0-20180603214616-504e848d77ea
-	github.com/dlclark/regexp2 v1.2.0 // indirect
 	github.com/docker/docker v1.4.2-0.20180625184442-8e610b2b55bf
 	github.com/dop251/goja v0.0.0-20200219165308-d1232e640a87
 	github.com/edsrzf/mmap-go v0.0.0-20160512033002-935e0e8a636c
+	github.com/etclabscore/go-openrpc-reflect v0.0.31
 	github.com/fatih/color v1.7.0
 	github.com/fjl/memsize v0.0.0-20180418122429-ca190fb6ffbc
 	github.com/gballet/go-libpcsclite v0.0.0-20190607065134-2772fd86a8ff
-	github.com/go-ole/go-ole v1.2.1 // indirect
-	github.com/go-sourcemap/sourcemap v2.1.2+incompatible // indirect
 	github.com/go-stack/stack v1.8.0
 	github.com/go-test/deep v1.0.5
 	github.com/golang/protobuf v1.3.2
 	github.com/golang/snappy v0.0.2-0.20190904063534-ff6b7dc882cf
-	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/graph-gophers/graphql-go v0.0.0-20191115155744-f33e81362277
 	github.com/hashicorp/golang-lru v0.5.4
@@ -39,11 +34,9 @@ require (
 	github.com/jackpal/go-nat-pmp v1.0.2-0.20160603034137-1fa385a6f458
 	github.com/julienschmidt/httprouter v1.2.0
 	github.com/karalabe/usb v0.0.0-20190919080040-51dc0efba356
-	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.0
 	github.com/mattn/go-isatty v0.0.5-0.20180830101745-3fb116b82035
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
 	github.com/olekukonko/tablewriter v0.0.2-0.20190409134802-7e037d187b0c
 	github.com/pborman/uuid v0.0.0-20170112150404-1b00554d8222
@@ -51,18 +44,16 @@ require (
 	github.com/prometheus/tsdb v0.7.1
 	github.com/rjeczalik/notify v0.9.1
 	github.com/rs/cors v0.0.0-20160617231935-a62a804a8a00
-	github.com/rs/xhandler v0.0.0-20160618193221-ed27b6fd6521 // indirect
 	github.com/shirou/gopsutil v2.20.5+incompatible
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
 	github.com/status-im/keycard-go v0.0.0-20190316090335-8537d3370df4
 	github.com/steakknife/bloomfilter v0.0.0-20180922174646-6819c0d2a570
-	github.com/steakknife/hamming v0.0.0-20180906055917-c99c65617cd3 // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/syndtr/goleveldb v1.0.1-0.20190923125748-758128399b1d
-	github.com/tidwall/gjson v1.3.5
+	github.com/tidwall/gjson v1.6.0
 	github.com/tidwall/pretty v1.0.0
-	github.com/tyler-smith/go-bip39 v0.0.0-20181017060643-dbb3b84ba2ef
+	github.com/tyler-smith/go-bip39 v1.0.1-0.20181017060643-dbb3b84ba2ef
 	github.com/wsddn/go-ecdh v0.0.0-20161211032359-48726bab9208
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
@@ -73,5 +64,6 @@ require (
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce
 	gopkg.in/olebedev/go-duktape.v3 v3.0.0-20200619000410-60c24ae608a6
 	gopkg.in/urfave/cli.v1 v1.20.0
-	gotest.tools v2.2.0+incompatible // indirect
 )
+
+replace github.com/open-rpc/meta-schema => github.com/meowsbits/meta-schema v0.0.43
---
+++ node/openrpc.go
@@ -0,0 +1,225 @@
+package node
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"go/ast"
+	"go/printer"
+	"go/token"
+	"math/big"
+	"reflect"
+
+	"github.com/alecthomas/jsonschema"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+const bytesD = `{
+"title": "bytes",
+"type": "string",
+"description": "Hex representation of a variable length byte array",
+"pattern": "^0x([a-fA-F0-9]?)+$"
+}`
+const integerD = `{
+          "title": "integer",
+          "type": "string",
+          "pattern": "^0x[a-fA-F0-9]+$",
+          "description": "Hex representation of the integer"
+        }`
+const commonAddressD = `{
+          "title": "keccak",
+          "type": "string",
+          "description": "Hex representation of a Keccak 256 hash POINTER",
+          "pattern": "^0x[a-fA-F\\d]{64}$"
+        }`
+const commonHashD = `{
+          "title": "keccak",
+          "type": "string",
+          "description": "Hex representation of a Keccak 256 hash",
+          "pattern": "^0x[a-fA-F\\d]{64}$"
+        }`
+const hexutilBytesD = `{
+          "title": "dataWord",
+          "type": "string",
+          "description": "Hex representation of some bytes",
+          "pattern": "^0x([a-fA-F\\d])+$"
+        }`
+const hexutilUintD = `{
+		"title": "uint",
+			"type": "string",
+			"description": "Hex representation of a uint",
+			"pattern": "^0x([a-fA-F\\d])+$"
+	}`
+const hexutilUint64D = `{
+          "title": "uint64",
+          "type": "string",
+          "description": "Hex representation of a uint64",
+          "pattern": "^0x([a-fA-F\\d])+$"
+        }`
+const blockNumberTagD = `{
+	     "title": "blockNumberTag",
+	     "type": "string",
+	     "description": "The block height description",
+	     "enum": [
+	       "earliest",
+	       "latest",
+	       "pending"
+	     ]
+		}`
+
+var blockNumberD = fmt.Sprintf(`{
+		"title": "blockNumberIdentifier",
+		"oneOf": [%s, %s]
+		}`, blockNumberTagD, hexutilUint64D)
+
+const requireCanonicalD = `{
+		  "type": "object",
+		  "properties": {
+			"requireCanonical": {
+			  "type": "boolean"
+			}
+		  },
+		  "additionalProperties": false
+		}`
+
+var blockNumberOrHashD = fmt.Sprintf(`{
+          "oneOf": [
+            %s,
+            {
+				"allOf": [%s, %s]
+			}
+          ]
+        }`, blockNumberD, commonHashD, requireCanonicalD)
+
+var emptyInterface interface{}
+
+// schemaDictEntry represents a type association passed to the jsonschema reflector.
+type schemaDictEntry struct {
+	example interface{}
+	rawJson string
+}
+
+// OpenRPCJSONSchemaTypeMapper contains the application-specific type mapping
+// passed to the jsonschema reflector, used in generating a runtime representation
+// of specific API objects.
+func OpenRPCJSONSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
+	unmarshalJSONToJSONSchemaType := func(input string) *jsonschema.Type {
+		var js jsonschema.Type
+		err := json.Unmarshal([]byte(input), &js)
+		if err != nil {
+			panic(err)
+		}
+		return &js
+	}
+
+	if ty.Kind() == reflect.Ptr {
+		ty = ty.Elem()
+	}
+
+	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {
+		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}
+	}
+
+	// Second, handle other types.
+	// Use a slice instead of a map because it preserves order, as a logic safeguard/fallback.
+	dict := []schemaDictEntry{
+		//{interface{}{}, fmt.Sprintf(`{
+		//	"oneOf": [{"additionalProperties": true}, {"type": "null"}]
+		//}`)},
+		{[]byte{}, bytesD},
+		{big.Int{}, integerD},
+		{types.BlockNonce{}, integerD},
+		{common.Address{}, commonAddressD},
+		{common.Hash{}, commonHashD},
+		{hexutil.Big{}, integerD},
+		{hexutil.Bytes{}, hexutilBytesD},
+		{hexutil.Uint(0), hexutilUintD},
+		{hexutil.Uint64(0), hexutilUint64D},
+		{rpc.BlockNumber(0), blockNumberD},
+		{rpc.BlockNumberOrHash{}, blockNumberOrHashD},
+
+		{rpc.Subscription{}, fmt.Sprintf(`{
+			"type": "object",
+			"title": "Subscription",
+			"summary": ""
+		}`)},
+	}
+
+	for _, d := range dict {
+		if reflect.TypeOf(d.example) == ty {
+			tt := unmarshalJSONToJSONSchemaType(d.rawJson)
+
+			return tt
+		}
+	}
+
+	// Handle primitive types in case there are generic cases
+	// specific to our services.
+	switch ty.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		// Return all integer types as the hex representation integer schemea.
+		ret := unmarshalJSONToJSONSchemaType(integerD)
+		return ret
+	case reflect.Struct:
+	case reflect.Map:
+	case reflect.Slice, reflect.Array:
+	case reflect.Float32, reflect.Float64:
+	case reflect.Bool:
+	case reflect.String:
+	case reflect.Ptr, reflect.Interface:
+	default:
+	}
+
+	return nil
+}
+
+func expandedFieldNamesFromList(in []*ast.Field) (out []*ast.Field) {
+	expandedFields := []*ast.Field{}
+	for _, f := range in {
+		expandedFields = append(expandedFields, fieldsWithNames(f)...)
+	}
+	return expandedFields
+}
+
+// fieldsWithNames expands a field (either parameter or result, in this case) to
+// fields which all have at least one name, or at least one field with one name.
+// This handles unnamed fields, and fields declared using multiple names with one type.
+// Unnamed fields are assigned a name that is the 'printed' identity of the field Type,
+// eg. int -> int, bool -> bool
+func fieldsWithNames(f *ast.Field) (fields []*ast.Field) {
+	if f == nil {
+		return nil
+	}
+
+	if len(f.Names) == 0 {
+		fields = append(fields, &ast.Field{
+			Doc:     f.Doc,
+			Names:   []*ast.Ident{{Name: printIdentField(f)}},
+			Type:    f.Type,
+			Tag:     f.Tag,
+			Comment: f.Comment,
+		})
+		return
+	}
+	for _, ident := range f.Names {
+		fields = append(fields, &ast.Field{
+			Doc:     f.Doc,
+			Names:   []*ast.Ident{ident},
+			Type:    f.Type,
+			Tag:     f.Tag,
+			Comment: f.Comment,
+		})
+	}
+	return
+}
+
+func printIdentField(f *ast.Field) string {
+	b := []byte{}
+	buf := bytes.NewBuffer(b)
+	fs := token.NewFileSet()
+	printer.Fprint(buf, fs, f.Type.(ast.Node))
+	return buf.String()
+}

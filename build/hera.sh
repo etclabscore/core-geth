16c518d2daa33206ccc111a022e3b6b105545df0
meows <b5c6@protonmail.com>
2020-07-07 14:49:53 -0500

    build/hera.sh,build/ssvm.sh: peg hera to 0.2.5
    Which is the last evmcv6-compat tag.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/hera.sh
+++ build/hera.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-mkdir -p build/_workspace
-[ ! -d build/_workspace/hera ] && git clone https://github.com/ewasm/hera build/_workspace/hera || echo "Hera exists."
-cd build/_workspace/hera
-git submodule update --init
-mkdir -p build
-cd build
-cmake -DBUILD_SHARED_LIBS=ON ..
-cmake --build .
-echo "Built library at: $(pwd)/src/libhera.so"
+main() {
+    mkdir -p build/_workspace
+    [ ! -d build/_workspace/hera ] && git clone https://github.com/ewasm/hera build/_workspace/hera || echo "Hera exists."
+    cd build/_workspace/hera
+    git checkout v0.2.5 # Last EVMCv6-compatible tag (v0.3.0 bumps to v7).
+    git submodule update --init
+    mkdir -p build
+    cd build
+    cmake -DBUILD_SHARED_LIBS=ON ..
+    cmake --build .
+    echo "Built library at: $(pwd)/src/libhera.so"
+}
+main

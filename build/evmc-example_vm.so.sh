6f38ba16146dba800ed8015012347eb566395ab6
meows <b5c6@protonmail.com>
2020-07-08 15:24:08 -0500

    build/evmc-example_vm.so.sh: add debug flag and comments
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/evmc-example_vm.so.sh
+++ build/evmc-example_vm.so.sh
@@ -3,9 +3,14 @@
 # Travis errors when trying to use the "normal" 'go generate' way of making this file.
 # I don't know why.
 
+set -x
+
 if [[ $CI == true ]] && [[ $TRAVIS == true ]]; then
+	echo "In Travis CI. Building SO with an adhoc Makefile."
 	# Use a temporary adhoc Makefile located in a child-dir of the evmc submodule to build the required example_vm.so file.
 	# Once finished, remove the adhoc Makefile.
+	# The important part of this command (vs. the 'go generate' command)
+	# is the -fPIC flags.
 	> ./evmc/bindings/go/evmc/Makefile \
 	echo -e 'example_vm.so:\n\tgcc -fPIC -shared ../../../examples/example_vm/example_vm.c -I../../../include -o example_vm.so'
 	make -C ./evmc/bindings/go/evmc/ example_vm.so

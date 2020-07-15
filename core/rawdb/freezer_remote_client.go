0a7df55fba33477696f894fd679a88fe2ab029cb
meows <b5c6@protonmail.com>
2020-07-15 18:23:26 -0500

    rawdb: rename ExternalRemoteFreezer->FreezerRemoteClient
    This was the name used by the original New_ func,
    and I think is the most descriptive.
    
    The business logic here is providing an RPC client
    in lieu of file r/w business. Although this client
    will communicate with an external server, I think its
    name is best fit to Client rather than the relationship
    it (the client) will have with an External server.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/rawdb/freezer_remote_client.go
+++ core/rawdb/freezer_remote_client.go
@@ -7,21 +7,21 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// ExternalRemoteFreezer is rpc client functionality for a freezer
-type ExternalRemoteFreezer struct {
+// FreezerRemoteClient is rpc client functionality for a freezer
+type FreezerRemoteClient struct {
 	client *rpc.Client
 	status string
 	quit   chan struct{}
 }
 
 // newFreezerRemoteClient constructs a rpc client to connect to a remote freezer
-func newFreezerRemoteClient(endpoint string, ipc bool) (*ExternalRemoteFreezer, error) {
+func newFreezerRemoteClient(endpoint string, ipc bool) (*FreezerRemoteClient, error) {
 	client, err := rpc.Dial(endpoint)
 	if err != nil {
 		return nil, err
 	}
 
-	extfreezer := &ExternalRemoteFreezer{
+	extfreezer := &FreezerRemoteClient{
 		client: client,
 	}
 
@@ -34,13 +34,13 @@ func newFreezerRemoteClient(endpoint string, ipc bool) (*ExternalRemoteFreezer, 
 	return extfreezer, nil
 }
 
-func (api *ExternalRemoteFreezer) pingVersion() (string, error) {
+func (api *FreezerRemoteClient) pingVersion() (string, error) {
 
 	return "version 1", nil
 }
 
 // Close terminates the chain freezer, unmapping all the data files.
-func (api *ExternalRemoteFreezer) Close() error {
+func (api *FreezerRemoteClient) Close() error {
 	var res string
 	err := api.client.Call(&res, "freezer_close")
 	return err
@@ -48,14 +48,14 @@ func (api *ExternalRemoteFreezer) Close() error {
 
 // HasAncient returns an indicator whether the specified ancient data exists
 // in the freezer.
-func (api *ExternalRemoteFreezer) HasAncient(kind string, number uint64) (bool, error) {
+func (api *FreezerRemoteClient) HasAncient(kind string, number uint64) (bool, error) {
 	var res bool
 	err := api.client.Call(&res, "freezer_hasAncient", kind, number)
 	return res, err
 }
 
 // Ancient retrieves an ancient binary blob from the append-only immutable files.
-func (api *ExternalRemoteFreezer) Ancient(kind string, number uint64) ([]byte, error) {
+func (api *FreezerRemoteClient) Ancient(kind string, number uint64) ([]byte, error) {
 	var res string
 	if err := api.client.Call(&res, "freezer_ancient", kind, number); err != nil {
 		return nil, err
@@ -64,14 +64,14 @@ func (api *ExternalRemoteFreezer) Ancient(kind string, number uint64) ([]byte, e
 }
 
 // Ancients returns the length of the frozen items.
-func (api *ExternalRemoteFreezer) Ancients() (uint64, error) {
+func (api *FreezerRemoteClient) Ancients() (uint64, error) {
 	var res uint64
 	err := api.client.Call(&res, "freezer_ancients")
 	return res, err
 }
 
 // AncientSize returns the ancient size of the specified category.
-func (api *ExternalRemoteFreezer) AncientSize(kind string) (uint64, error) {
+func (api *FreezerRemoteClient) AncientSize(kind string) (uint64, error) {
 	var res uint64
 	err := api.client.Call(&res, "freezer_ancientSize", kind)
 	return res, err
@@ -85,7 +85,7 @@ func (api *ExternalRemoteFreezer) AncientSize(kind string) (uint64, error) {
 // the same time, we can get into the trouble.
 //
 // Note that the frozen marker is updated outside of the service calls.
-func (api *ExternalRemoteFreezer) AppendAncient(number uint64, hash, header, body, receipts, td []byte) (err error) {
+func (api *FreezerRemoteClient) AppendAncient(number uint64, hash, header, body, receipts, td []byte) (err error) {
 	var res string
 	hexHash := hexutil.Encode(hash)
 	hexHeader := hexutil.Encode(header)
@@ -97,19 +97,19 @@ func (api *ExternalRemoteFreezer) AppendAncient(number uint64, hash, header, bod
 }
 
 // TruncateAncients discards any recent data above the provided threshold number.
-func (api *ExternalRemoteFreezer) TruncateAncients(items uint64) error {
+func (api *FreezerRemoteClient) TruncateAncients(items uint64) error {
 	var res string
 	return api.client.Call(&res, "freezer_truncateAncients", items)
 }
 
 // Sync flushes all data tables to disk.
-func (api *ExternalRemoteFreezer) Sync() error {
+func (api *FreezerRemoteClient) Sync() error {
 	var res string
 	return api.client.Call(&res, "freezer_sync")
 }
 
 // repair truncates all data tables to the same length.
-func (api *ExternalRemoteFreezer) repair() error {
+func (api *FreezerRemoteClient) repair() error {
 	/*min := uint64(math.MaxUint64)
 	for _, table := range f.tables {
 		items := atomic.LoadUint64(&table.items)

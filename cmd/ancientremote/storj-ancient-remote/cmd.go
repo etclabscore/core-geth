a0b11d9c9ce3bd676b708ec044adfb940657d484
Zane Starr <zcstarr@gmail.com>
2020-07-28 10:10:30 -0700

    storj-ancient-remote: add support for storj (wip)

---
+++ cmd/ancientremote/storj-ancient-remote/cmd.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
+	"gopkg.in/urfave/cli.v1"
+)
+
+var (
+	// LogLevelFlag sets log level for server
+	LogLevelFlag = cli.IntFlag{
+		Name:  "loglevel",
+		Value: 4,
+		Usage: "log level to emit to the screen",
+	}
+	// RPCPortFlag sets port for http operation
+	RPCPortFlag = cli.IntFlag{
+		Name:  "rpcport",
+		Usage: "HTTP-RPC server listening port",
+		Value: 9797,
+	}
+	// IPCPathFlag sets ipc path for an ipc rpc server
+	IPCPathFlag = utils.DirectoryFlag{
+		Name:  "ipcpath",
+		Usage: "Filename for IPC socket/pipe within the datadir (explicit paths escape it)",
+	}
+	// HTTPListenAddrFlag sets address http address to listen on
+	HTTPListenAddrFlag = cli.StringFlag{
+		Name:  "http.addr",
+		Usage: "HTTP-RPC server listening interface",
+		Value: "localhost",
+	}
+
+	// BucketNameFlag sets namespace for S3 bucket
+	BucketNameFlag = cli.StringFlag{
+		Name:  "bucket",
+		Usage: "Storj bucket name",
+	}
+)
+
+func checkBucketArg(c *cli.Context) (bucketName string) {
+	bucketName = c.GlobalString(BucketNameFlag.Name)
+	if bucketName == "" {
+		utils.Fatalf("Missing bucket please specify a bucket, with --bucket")
+	}
+	return
+}
+
+func setupLogFormat(c *cli.Context) error {
+	// Set up the logger to print everything
+	logOutput := os.Stdout
+	usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
+	output := io.Writer(logOutput)
+	if usecolor {
+		output = colorable.NewColorable(logOutput)
+	}
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(c.Int(LogLevelFlag.Name)), log.StreamHandler(output, log.TerminalFormat(usecolor))))
+
+	return nil
+}
---
+++ cmd/ancientremote/storj-ancient-remote/freezer_remote_storj_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+func TestDifficultyRLPDecoding(t *testing.T) {
+	td := big.NewInt(42)
+	b, err := rlp.EncodeToBytes(td)
+	if err != nil {
+		t.Fatal(err)
+	}
+	difficulty := new(big.Int)
+	err = rlp.DecodeBytes(b, difficulty)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(difficulty)
+}
+
+func TestHashRLPCodec(t *testing.T) {
+	h1 := common.HexToHash("0xbadface")
+	b, err := rlp.EncodeToBytes(h1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	h2 := common.Hash{}
+	err = rlp.DecodeBytes(b, &h2)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(h2.Hex())
+}
+
+func TestBlockKeying(t *testing.T) {
+	m := 32
+	n := 17
+	mod := n % m
+	div := n / m
+	t.Log(mod)
+	t.Log(div)
+	t.Log(n*div + mod)
+}
+
+func TestIndexThing(t *testing.T) {
+	a := []int{1, 2, 3}
+	t.Log(a[:0])
+}
+
+func TestSpliceBackwards(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5}
+	mod := 3
+	rem := len(a) % mod
+	t.Log(a[len(a)-rem:])
+}
+
+func TestSlice(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5}
+	t.Log(a[2:5])
+}
+
+func Test30kHashes(t *testing.T) {
+	n := 32 * 32 * 32
+	hashes := make([]common.Hash, n)
+	for i := 0; i < n; i++ {
+		hashes[i] = common.HexToHash("0xbadface")
+	}
+	b, err := json.Marshal(hashes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := bytes.Count(b, []byte(""))
+	t.Log(c)
+}
+
+func TestCache_TruncateFrom(t *testing.T) {
+	/*c := newCache()
+	for i := 0; i < 32; i++ {
+		c.add(uint64(i), fmt.Sprintf("n%d", i))
+	}
+	c.truncateFrom(32)
+	b := c.batch(0, 36)
+	t.Log(c.sl, b)
+	*/
+}

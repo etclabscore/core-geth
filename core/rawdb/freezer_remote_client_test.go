6687708e66292f23099de85c8f9c1b2487e8a39b
meows <b5c6@protonmail.com>
2020-07-29 09:04:15 -0500

    core,rawdb: update tests to use logic in mock freezer server package
    Adds (duplicating) blockchain tests using IPC freezer server
    instead of tempfile ancient db.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/rawdb/freezer_remote_client_test.go
+++ core/rawdb/freezer_remote_client_test.go
@@ -2,114 +2,15 @@ package rawdb
 
 import (
 	"bytes"
-	"fmt"
-	"strconv"
-	"strings"
-	"sync"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/cmd/ancientremote/mock-ancient-remote/lib"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// MockFreezerRemoteServerAPI is a mock freezer server implementation.
-type MockFreezerRemoteServerAPI struct {
-	store map[string][]byte
-	count uint64
-	mu    sync.Mutex
-}
-
-func NewMockFreezerRemoteServerAPI() *MockFreezerRemoteServerAPI {
-	return &MockFreezerRemoteServerAPI{store: make(map[string][]byte)}
-}
-
-func (r *MockFreezerRemoteServerAPI) storeKey(kind string, number uint64) string {
-	return fmt.Sprintf("%s-%d", kind, number)
-}
-
-func (f *MockFreezerRemoteServerAPI) HasAncient(kind string, number uint64) (bool, error) {
-	fmt.Println("mock server called", "method=HasAncient")
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	_, ok := f.store[f.storeKey(kind, number)]
-	return ok, nil
-}
-
-func (f *MockFreezerRemoteServerAPI) Ancient(kind string, number uint64) ([]byte, error) {
-	fmt.Println("mock server called", "method=Ancient")
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	v, ok := f.store[f.storeKey(kind, number)]
-	if !ok {
-		return nil, errOutOfBounds
-	}
-	return v, nil
-}
-
-func (f *MockFreezerRemoteServerAPI) Ancients() (uint64, error) {
-	fmt.Println("mock server called", "method=Ancients")
-	return f.count, nil
-}
-
-func (f *MockFreezerRemoteServerAPI) AncientSize(kind string) (uint64, error) {
-	fmt.Println("mock server called", "method=AncientSize")
-	sum := uint64(0)
-	for k, v := range f.store {
-		if strings.HasPrefix(k, kind) {
-			sum += uint64(len(v))
-		}
-	}
-	return sum, nil
-}
-
-func (f *MockFreezerRemoteServerAPI) AppendAncient(number uint64, hash, header, body, receipt, td []byte) error {
-	fmt.Println("mock server called", "method=AppendAncient", "number=", number, "header", fmt.Sprintf("%x", header))
-	fieldNames := []string{FreezerRemoteHashTable, FreezerRemoteHeaderTable,
-		FreezerRemoteBodiesTable, FreezerRemoteReceiptTable, FreezerRemoteDifficultyTable}
-	fields := [][]byte{hash, header, body, receipt, td}
-	if number != f.count {
-		return errOutOrderInsertion
-	}
-	f.count = number + 1
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	for i, fv := range fields {
-		kind := fieldNames[i]
-		f.store[f.storeKey(kind, number)] = fv
-	}
-	return nil
-}
-
-func (f *MockFreezerRemoteServerAPI) TruncateAncients(n uint64) error {
-	fmt.Println("mock server called", "method=TruncateAncients")
-	f.count = n
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	for k := range f.store {
-		spl := strings.Split(k, "-")
-		num, err := strconv.ParseUint(spl[1], 10, 64)
-		if err != nil {
-			return err
-		}
-		if num >= n {
-			delete(f.store, k)
-		}
-	}
-	return nil
-}
-
-func (f *MockFreezerRemoteServerAPI) Sync() error {
-	fmt.Println("mock server called", "method=Sync")
-	return nil
-}
-
-func (f *MockFreezerRemoteServerAPI) Close() error {
-	fmt.Println("mock server called", "method=Close")
-	return nil
-}
-
 func newTestServer(t *testing.T) *rpc.Server {
 	server := rpc.NewServer()
-	mockFreezerServer := NewMockFreezerRemoteServerAPI()
+	mockFreezerServer := lib.NewMockFreezerRemoteServerAPI()
 	err := server.RegisterName("freezer", mockFreezerServer)
 	if err != nil {
 		t.Fatal(err)

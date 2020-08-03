9ad77142358a9bdb56d21b0998b28b0ebbcb4e3b
meows <b5c6@protonmail.com>
2020-08-03 17:32:55 -0500

    miner: fix import path for Ether constant
    Signed-off-by: meows <b5c6@protonmail.com>

--- miner/worker.go
+++ miner/worker.go
@@ -1034,5 +1034,5 @@ func totalFees(block *types.Block, receipts []*types.Receipt) *big.Float {
 	for i, tx := range block.Transactions() {
 		feesWei.Add(feesWei, new(big.Int).Mul(new(big.Int).SetUint64(receipts[i].GasUsed), tx.GasPrice()))
 	}
-	return new(big.Float).Quo(new(big.Float).SetInt(feesWei), new(big.Float).SetInt(big.NewInt(params.Ether)))
+	return new(big.Float).Quo(new(big.Float).SetInt(feesWei), new(big.Float).SetInt(big.NewInt(vars.Ether)))
 }

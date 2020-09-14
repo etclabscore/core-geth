e35f1ed4c26c148feaa588f90957bd9b12482860
Luke Williams <luke@iquidus.io>
2020-09-14 20:45:18 +0200

    ecip-1099: update algorithm_test

--- consensus/ethash/algorithm_test.go
+++ consensus/ethash/algorithm_test.go
@@ -108,7 +108,7 @@ func TestCacheGeneration(t *testing.T) {
 	}
 	for i, tt := range tests {
 		cache := make([]uint32, tt.size/4)
-		generateCache(cache, tt.epoch, seedHash(tt.epoch*oldEpochLength+1))
+		generateCache(cache, tt.epoch, oldEpochLength, seedHash(tt.epoch*oldEpochLength+1))
 
 		want := make([]uint32, tt.size/4)
 		prepare(want, tt.cache)
@@ -648,10 +648,10 @@ func TestDatasetGeneration(t *testing.T) {
 	}
 	for i, tt := range tests {
 		cache := make([]uint32, tt.cacheSize/4)
-		generateCache(cache, tt.epoch, seedHash(tt.epoch*oldEpochLength+1))
+		generateCache(cache, tt.epoch, oldEpochLength, seedHash(tt.epoch*oldEpochLength+1))
 
 		dataset := make([]uint32, tt.datasetSize/4)
-		generateDataset(dataset, tt.epoch, cache)
+		generateDataset(dataset, tt.epoch, oldEpochLength, cache)
 
 		want := make([]uint32, tt.datasetSize/4)
 		prepare(want, tt.dataset)
@@ -667,10 +667,10 @@ func TestDatasetGeneration(t *testing.T) {
 func TestHashimoto(t *testing.T) {
 	// Create the verification cache and mining dataset
 	cache := make([]uint32, 1024/4)
-	generateCache(cache, 0, make([]byte, 32))
+	generateCache(cache, 0, oldEpochLength, make([]byte, 32))
 
 	dataset := make([]uint32, 32*1024/4)
-	generateDataset(dataset, 0, cache)
+	generateDataset(dataset, 0, oldEpochLength, cache)
 
 	// Create a block to verify
 	hash := hexutil.MustDecode("0xc9149cc0386e689d789a1c2f3d5d169a61a6218ed30e74414dc736e442ef3d1f")
@@ -743,26 +743,26 @@ func TestConcurrentDiskCacheGeneration(t *testing.T) {
 func BenchmarkCacheGeneration(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		cache := make([]uint32, cacheSize(1)/4)
-		generateCache(cache, 0, make([]byte, 32))
+		generateCache(cache, 0, oldEpochLength, make([]byte, 32))
 	}
 }
 
 // Benchmarks the dataset (small) generation performance.
 func BenchmarkSmallDatasetGeneration(b *testing.B) {
 	cache := make([]uint32, 65536/4)
-	generateCache(cache, 0, make([]byte, 32))
+	generateCache(cache, 0, oldEpochLength, make([]byte, 32))
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		dataset := make([]uint32, 32*65536/4)
-		generateDataset(dataset, 0, cache)
+		generateDataset(dataset, 0, oldEpochLength, cache)
 	}
 }
 
 // Benchmarks the light verification performance.
 func BenchmarkHashimotoLight(b *testing.B) {
 	cache := make([]uint32, cacheSize(1)/4)
-	generateCache(cache, 0, make([]byte, 32))
+	generateCache(cache, 0, oldEpochLength, make([]byte, 32))
 
 	hash := hexutil.MustDecode("0xc9149cc0386e689d789a1c2f3d5d169a61a6218ed30e74414dc736e442ef3d1f")
 
@@ -775,10 +775,10 @@ func BenchmarkHashimotoLight(b *testing.B) {
 // Benchmarks the full (small) verification performance.
 func BenchmarkHashimotoFullSmall(b *testing.B) {
 	cache := make([]uint32, 65536/4)
-	generateCache(cache, 0, make([]byte, 32))
+	generateCache(cache, 0, oldEpochLength, make([]byte, 32))
 
 	dataset := make([]uint32, 32*65536/4)
-	generateDataset(dataset, 0, cache)
+	generateDataset(dataset, oldEpochLength, 0, cache)
 
 	hash := hexutil.MustDecode("0xc9149cc0386e689d789a1c2f3d5d169a61a6218ed30e74414dc736e442ef3d1f")
 

aa4f4dd630cf3994b8aeef9a781d9ffc55a086e6
meows <b5c6@protonmail.com>
2020-09-21 17:32:29 -0500

    params: bump version to 1.11.14-rc-messnet-6
    Signed-off-by: meows <b5c6@protonmail.com>

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1               // Major version component of the current release
-	VersionMinor = 11              // Minor version component of the current release
-	VersionPatch = 14              // Patch version component of the current release
-	VersionMeta  = "rc-messnet-5+" // Version metadata to append to the version string
+	VersionMajor = 1              // Major version component of the current release
+	VersionMinor = 11             // Minor version component of the current release
+	VersionPatch = 14             // Patch version component of the current release
+	VersionMeta  = "rc-messnet-6" // Version metadata to append to the version string
 	VersionName  = "CoreGeth"
 )
 

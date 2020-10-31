f9da3e4e000c2e4762645ca1e993a373f4668ce1
meows <b5c6@protonmail.com>
2020-10-31 07:00:30 -0500

    node: refactor openrpc specific logic to openrpc file
    This moves code that is directly and only related
    to OpenRPC documentation to node/openrpc.go file.
    
    This will help reduce merge conflicts at node/node.go
    and keeps same-domain logic grouped together.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- node/node.go
+++ node/node.go
@@ -17,29 +17,22 @@
 package node
 
 import (
-	"context"
 	"errors"
 	"fmt"
-	"go/ast"
-	"net"
 	"net/http"
 	"os"
 	"path/filepath"
 	"reflect"
-	"regexp"
 	"strings"
 	"sync"
 
-	"github.com/alecthomas/jsonschema"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rpc"
-	meta_schema "github.com/open-rpc/meta-schema"
 	"github.com/prometheus/tsdb/fileutil"
 
 	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"
@@ -78,139 +71,6 @@ const (
 	runningState
 	closedState
 )
-
-// RPCDiscoveryService defines a receiver type used for RPC discovery by reflection.
-type RPCDiscoveryService struct {
-	d *go_openrpc_reflect.Document
-}
-
-// Discover exposes a Discover method to the RPC receiver registration.
-func (r *RPCDiscoveryService) Discover() (*meta_schema.OpenrpcDocument, error) {
-	return r.d.Discover()
-}
-
-// newOpenRPCDocument returns a Document configured with application-specific logic.
-func newOpenRPCDocument() *go_openrpc_reflect.Document {
-	d := &go_openrpc_reflect.Document{}
-
-	// Register "Meta" document fields.
-	// These include getters for
-	// - Servers object
-	// - Info object
-	// - ExternalDocs object
-	//
-	// These objects represent server-specific data that cannot be
-	// reflected.
-	d.WithMeta(&go_openrpc_reflect.MetaT{
-		GetServersFn: func() func(listeners []net.Listener) (*meta_schema.Servers, error) {
-			return func(listeners []net.Listener) (*meta_schema.Servers, error) {
-				servers := []meta_schema.ServerObject{}
-				for _, listener := range listeners {
-					addr := "http://" + listener.Addr().String()
-					network := listener.Addr().Network()
-					servers = append(servers, meta_schema.ServerObject{
-						Url:  (*meta_schema.ServerObjectUrl)(&addr),
-						Name: (*meta_schema.ServerObjectName)(&network),
-					})
-				}
-				return (*meta_schema.Servers)(&servers), nil
-			}
-		},
-		GetInfoFn: func() (info *meta_schema.InfoObject) {
-			info = &meta_schema.InfoObject{}
-			title := "Core-Geth RPC API"
-			info.Title = (*meta_schema.InfoObjectProperties)(&title)
-
-			version := params.VersionWithMeta
-			info.Version = (*meta_schema.InfoObjectVersion)(&version)
-			return info
-		},
-		GetExternalDocsFn: func() (exdocs *meta_schema.ExternalDocumentationObject) {
-			return nil // FIXME
-		},
-	})
-
-	// Use a provided Ethereum default configuration as a base.
-	appReflector := &go_openrpc_reflect.EthereumReflectorT{}
-
-	// Install overrides for the json schema->type map fn used by the jsonschema reflect package.
-	appReflector.FnSchemaTypeMap = func() func(ty reflect.Type) *jsonschema.Type {
-		return OpenRPCJSONSchemaTypeMapper
-	}
-
-	// Install an override for method eligibility to exclude subscription methods.
-	// The majority of this logic is taken from the go_openrpc_reflect package configuration default,
-	// with the clause commented 'Custom' noting the custom logic.
-	var errType = reflect.TypeOf((*error)(nil)).Elem()
-	var contextType = reflect.TypeOf((*context.Context)(nil)).Elem()
-	appReflector.FnIsMethodEligible = func(method reflect.Method) bool {
-
-		// Method must be exported.
-		if method.PkgPath != "" {
-			return false
-		}
-
-		// Custom: skip methods with 1 argument of context.Context.
-		// We'll consider these methods subscription methods,
-		// and they are not well supported by OpenRPC.
-		if method.Type.NumIn() == 2 {
-			if method.Type.In(1) == contextType {
-				return false
-			}
-		}
-
-		// Verify return types. The function must return at most one error
-		// and/or one other non-error value.
-		outs := make([]reflect.Type, method.Func.Type().NumOut())
-		for i := 0; i < method.Func.Type().NumOut(); i++ {
-			outs[i] = method.Func.Type().Out(i)
-		}
-		isErrorType := func(ty reflect.Type) bool {
-			return ty == errType
-		}
-
-		// If an error is returned, it must be the last returned value.
-		switch {
-		case len(outs) > 2:
-			return false
-		case len(outs) == 1 && isErrorType(outs[0]):
-			return true
-		case len(outs) == 2:
-			if isErrorType(outs[0]) || !isErrorType(outs[1]) {
-				return false
-			}
-		}
-		return true
-	}
-
-	appReflector.FnGetContentDescriptorName = func(r reflect.Value, m reflect.Method, field *ast.Field) (string, error) {
-		fs := expandedFieldNamesFromList([]*ast.Field{field})
-		name := fs[0].Names[0].Name
-		// removeChars are characters that look like code.
-		// Shane doesn't like these because they might be weird for generated clients to use
-		// as variable/field names (eg for params-by-name stuff).
-		removeChars := ".*[]{}-"
-		for _, c := range strings.Split(removeChars, "") {
-			name = strings.ReplaceAll(name, c, "")
-		}
-		if regexp.MustCompile(`(?m)^\d`).MatchString(name) {
-			name = "num" + name
-		}
-		return name, nil
-	}
-
-	// Finally, register the configured reflector to the document.
-	d.WithReflector(appReflector)
-	return d
-}
-
-// registerOpenRPCAPIs provides a convenience logic that is reused
-// congruent to the rpc package receiver registrations.
-func registerOpenRPCAPIs(doc *go_openrpc_reflect.Document, apis []rpc.API) {
-	for _, api := range apis {
-		doc.RegisterReceiverName(api.Namespace, api.Service)
-	}
-}
 
 // New creates a new P2P node, ready for protocol registration.
 func New(conf *Config) (*Node, error) {

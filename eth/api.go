f92ec2640b3762fb09b172287813529eb242a573
meows <b5c6@protonmail.com>
2020-11-04 05:20:38 -0600

    eth: fix comment to meet go convention
    Signed-off-by: meows <b5c6@protonmail.com>

--- eth/api.go
+++ eth/api.go
@@ -543,7 +543,7 @@ func (api *PrivateDebugAPI) getModifiedAccounts(startBlock, endBlock *types.Bloc
 	return dirty, nil
 }
 
-// RemoveTransaction removes a transaction from the txpool.
+// RemovePendingTransaction removes a transaction from the txpool.
 // It returns the transaction removed, if any.
 func (api *PrivateDebugAPI) RemovePendingTransaction(hash common.Hash) (*types.Transaction, error) {
 	return api.eth.txPool.RemoveTx(hash), nil

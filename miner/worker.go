f1c992d46f784ad14050876bc2e01c9855db5357
Chris Ziogas <ziogaschr@gmail.com>
2020-12-30 00:03:50 +0200

    miner: change log level for out of scope message when not mining

--- miner/worker.go
+++ miner/worker.go
@@ -861,7 +861,11 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 	// this will ensure we're not going off too far in the future
 	if now := time.Now().Unix(); timestamp > now+1 {
 		wait := time.Duration(timestamp-now) * time.Second
-		log.Info("Mining too far in the future", "wait", common.PrettyDuration(wait))
+		logFn := log.Trace
+		if w.isRunning() {
+			logFn = log.Info
+		}
+		logFn("Mining too far in the future", "wait", common.PrettyDuration(wait))
 		time.Sleep(wait)
 	}
 

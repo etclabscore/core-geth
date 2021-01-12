6ae8387e1a45a407fdb12a2c30dc368fa001b20e
meows <b5c6@protonmail.com>
2021-01-11 18:13:48 -0600

    eth/filters,ethclient,node: install newSideHeads subscription
    The newSideHeads subscription work very similarly to
    the newHeads subscription; instead, non-canonical blocks are channeled.

--- eth/filters/filter.go
+++ eth/filters/filter.go
@@ -39,6 +39,7 @@ type Backend interface {
 
 	SubscribeNewTxsEvent(chan<- core.NewTxsEvent) event.Subscription
 	SubscribeChainEvent(ch chan<- core.ChainEvent) event.Subscription
+	SubscribeChainSideEvent(ch chan<- core.ChainSideEvent) event.Subscription
 	SubscribeRemovedLogsEvent(ch chan<- core.RemovedLogsEvent) event.Subscription
 	SubscribeLogsEvent(ch chan<- []*types.Log) event.Subscription
 	SubscribePendingLogsEvent(ch chan<- []*types.Log) event.Subscription

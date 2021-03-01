fd578856e5c209ec51622e0215b8550f33fbd67b
meows <b5c6@protonmail.com>
2021-03-01 10:57:00 -0600

    cmd/geth: add test showing detailed use cases for chain default/nondefaults
    Date: 2021-03-01 10:57:00-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/geth/consolecmd_test.go
+++ cmd/geth/consolecmd_test.go
@@ -120,32 +120,86 @@ func TestGethFailureToLaunch(t *testing.T) {
 // TestGethStartupLogs tests that geth logs certain things (given some set of flags).
 // In these cases, geth is run with a console command to print its name (and tests that it does).
 func TestGethStartupLogs(t *testing.T) {
+	// semiPersistentDatadir is used to house an adhoc datadir for co-dependent geth test cases.
+	semiPersistentDatadir := filepath.Join(os.TempDir(), fmt.Sprintf("geth-startup-logs-test-%d", time.Now().Unix()))
+	defer os.RemoveAll(semiPersistentDatadir)
+
+	type matching struct {
+		pattern string // pattern is the pattern to match against geth's stderr log.
+		matches bool   // matches defines if the pattern should succeed or fail, ie. if the pattern should exist or should not exist.
+	}
 	cases := []struct {
-		flags              []string
-		expectStdErrRegexp string
+		flags    []string
+		matchers []matching
+
+		// callback is run after the geth run case completes.
+		// It can be used to reset any persistent state to provide a clean slate for the subsequent cases.
+		callback func() error
 	}{
 		{
-			// --<chain> flag is NOT given. This is deprecated. User will be warned.
-			flags:              []string{},
-			expectStdErrRegexp: "(?ism).+WARN.+Not specifying a chain flag is deprecated.*",
+			// --<chain> flag is NOT given and datadir does not exist, representing a first tabula-rasa run.
+			// Use without a --<chain> flag is deprecated. User will be warned.
+			flags: []string{},
+			matchers: []matching{
+				{pattern: "(?ism).+WARN.+Not specifying a chain flag is deprecated.*", matches: true},
+			},
 		},
 		{
 			// Network flag is given.
 			// --<chain> flag is NOT given. This is deprecated. User will be warned.
-			flags:              []string{"--networkid=42"},
-			expectStdErrRegexp: "(?ism).+WARN.+Not specifying a chain flag is deprecated.*",
+			// Same same but different as above.
+			flags: []string{"--networkid=42"},
+			matchers: []matching{
+				{pattern: "(?ism).+WARN.+Not specifying a chain flag is deprecated.*", matches: true},
+			},
+		},
+		// Little bit of a HACK.
+		// This is a co-dependent sequence of two test cases.
+		// First, startup a geth instance that will create a database, storing the genesis block.
+		// This is a basic use case and has no errors.
+		// The subsequent case then run geth re-using that datadir which has an existing chain database
+		// and contains a stored genesis block.
+		// Since the database contains a genesis block, the chain identity and config can (and will) be deduced from it;
+		// this causes no need for a --<chain> CLI flag to be passed again. The user will not be warned of a missing --<chain> flag.
+		{
+			// --<chain> flag is given. All is well. Database (storing genesis) is initialized.
+			flags: []string{"--datadir", semiPersistentDatadir, "--mainnet"},
+			matchers: []matching{
+				{pattern: "(?ism).*", matches: true},
+			},
+		},
+		{
+			// --<chain> flag is NOT given, BUT geth is being run on top of an existing
+			// datadir. Geth will use the existing (stored) genesis found in it.
+			// User should NOT be warned.
+			flags: []string{"--datadir", semiPersistentDatadir},
+			matchers: []matching{
+				{pattern: "(?ism).+WARN.+Not specifying a chain flag is deprecated.*", matches: false},
+				{pattern: "(?ism).+INFO.+Found stored genesis block.*", matches: true},
+			},
+			callback: func() error {
+				// Clean up this mini-suite.
+				return os.RemoveAll(semiPersistentDatadir)
+			},
 		},
 	}
-	for _, c := range cases {
-		t.Run(fmt.Sprintf("TestGethStartupLogs: %v", c.flags), func(t *testing.T) {
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("TestGethStartupLogs/%d: %v", i, c.flags), func(t *testing.T) {
 			geth := runGeth(t, append(c.flags, "--exec", "admin.nodeInfo.name", "console")...)
 			geth.ExpectRegexp("(?ism).*CoreGeth.*")
 			geth.ExpectExit()
 			if status := geth.ExitStatus(); status != 0 {
 				t.Errorf("expected exit status == 0, got: %d", status)
 			}
-			if !regexp.MustCompile(c.expectStdErrRegexp).MatchString(geth.StderrText()) {
-				t.Errorf("unmatched stderr output; want: %s, got: %s", c.expectStdErrRegexp, geth.StderrText())
+			for _, match := range c.matchers {
+				if matched := regexp.MustCompile(match.pattern).MatchString(geth.StderrText()); matched != match.matches {
+					t.Errorf("unexpected stderr output; want: %s (matching?=%v) got: %s", match.pattern, match.matches, geth.StderrText())
+				}
+			}
+			if c.callback != nil {
+				if err := c.callback(); err != nil {
+					t.Fatal(err)
+				}
 			}
 		})
 	}

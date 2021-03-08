c9973290345b311b670f18c5849deb0f9495a2d6
meows <b5c6@protonmail.com>
2021-03-08 10:13:42 -0600

    build: bump aleth-interpreter 1.6.0-alpha.1 -> 1.8.0
    Date: 2021-03-08 10:13:42-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/aleth-interpreter.sh
+++ build/aleth-interpreter.sh
@@ -3,5 +3,5 @@
 set -e
 
 mkdir -p build/_workspace/aleth
-wget -O build/_workspace/aleth/aleth-1.6.0-alpha.1-linux-x86_64.tar.gz https://github.com/ethereum/aleth/releases/download/v1.6.0-alpha.1/aleth-1.6.0-alpha.1-linux-x86_64.tar.gz
-tar xzvf build/_workspace/aleth/aleth-1.6.0-alpha.1-linux-x86_64.tar.gz -C build/_workspace/aleth/
+wget -O build/_workspace/aleth/aleth-1.8.0-linux-x86_64.tar.gz https://github.com/ethereum/aleth/releases/download/v1.8.0/aleth-1.8.0-linux-x86_64.tar.gz
+tar xzvf build/_workspace/aleth/aleth-1.8.0-linux-x86_64.tar.gz -C build/_workspace/aleth/

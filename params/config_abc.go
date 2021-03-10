3d52cf2b0cfa888c6f858b331d4ccc2288183d52
meows <b5c6@protonmail.com>
2021-03-10 08:17:14 -0600

    params: init ABCChainConfig
    This is a basic, initial draft of a fairly
    clean-slate chain configuration.
    
    Date: 2021-03-10 08:17:14-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

---
+++ params/config_abc.go
@@ -0,0 +1,79 @@
+// Copyright 2019 The multi-geth Authors
+// This file is part of the multi-geth library.
+//
+// The multi-geth library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The multi-geth library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the multi-geth library. If not, see <http://www.gnu.org/licenses/>.
+package params
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/params/types/coregeth"
+	"github.com/ethereum/go-ethereum/params/types/ctypes"
+)
+
+var (
+	// MordorChainConfig is the chain parameters to run a node on the Ethereum Classic Mordor test network (PoW).
+	ABCChainConfig = &coregeth.CoreGethChainConfig{
+		NetworkID: 4269,
+		ChainID:   big.NewInt(4269),
+		Ethash:    new(ctypes.EthashConfig),
+
+		EIP2FBlock: big.NewInt(0),
+		EIP7FBlock: big.NewInt(0),
+
+		EIP150Block: big.NewInt(0),
+
+		EIP155Block: big.NewInt(0),
+
+		// EIP158 eq
+		EIP160FBlock: big.NewInt(0),
+		EIP161FBlock: big.NewInt(0),
+		EIP170FBlock: big.NewInt(0),
+
+		// Byzantium eq
+		EIP100FBlock: big.NewInt(0),
+		EIP140FBlock: big.NewInt(0),
+		EIP198FBlock: big.NewInt(0),
+		EIP211FBlock: big.NewInt(0),
+		EIP212FBlock: big.NewInt(0),
+		EIP213FBlock: big.NewInt(0),
+		EIP214FBlock: big.NewInt(0),
+		EIP658FBlock: big.NewInt(0),
+
+		// Constantinople eq, aka Agharta
+		EIP145FBlock:  big.NewInt(0),
+		EIP1014FBlock: big.NewInt(0),
+		EIP1052FBlock: big.NewInt(0),
+
+		// Istanbul eq, aka Phoenix
+		// ECIP-1088
+		EIP152FBlock:  big.NewInt(0),
+		EIP1108FBlock: big.NewInt(0),
+		EIP1344FBlock: big.NewInt(0),
+		EIP1884FBlock: big.NewInt(0),
+		EIP2028FBlock: big.NewInt(0),
+		EIP2200FBlock: big.NewInt(0), // RePetersburg (== re-1283)
+
+		ECIP1099FBlock: nil, // Etchash
+
+		DisposalBlock:      big.NewInt(0), // Dispose difficulty bomb
+		ECIP1017FBlock:     nil,           // Ethereum Classic's disinflationary monetary policy
+		ECIP1017EraRounds:  nil,
+		ECIP1010PauseBlock: nil, // No need to delay difficulty bomb, is defused by default
+		ECIP1010Length:     nil,
+		ECBP1100FBlock:     nil, // ECBP1100 (MESS artificial finality)
+		RequireBlockHashes: map[uint64]common.Hash{},
+	}
+)

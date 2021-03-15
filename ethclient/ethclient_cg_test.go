3b7de793ff39d16d77450cffebc42f3c23480f16
meows <b5c6@protonmail.com>
2021-03-15 15:23:31 -0500

    ethclient: use go subtest syntax for better descriptiveness and logging
    Date: 2021-03-15 15:23:31-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- ethclient/ethclient_cg_test.go
+++ ethclient/ethclient_cg_test.go
@@ -46,111 +46,77 @@ func TestEthGetBlockByNumber_ValidJSONResponse_CanCompareGoEthereum(t *testing.T
 	reHexAnyLen := regexp.MustCompile(`^"0x[a-zA-Z0-9]+"$`)
 	reHexHashLen := regexp.MustCompile(fmt.Sprintf(`^"0x[a-zA-Z0-9]{%d}"$`, common.HashLength*2))
 
+	// completeBlockExpectations define expectations for 'earliest' and 'latest' blocks.
+	completeBlockExpectations := map[string]*regexp.Regexp{
+		"nonce": reHexAnyLen,
+		"hash":  reHexAnyLen,
+		"miner": regexp.MustCompile(fmt.Sprintf(`^"0x[a-zA-Z0-9]{%d}"$`, common.AddressLength*2)),
+
+		"totalDifficulty": reHexAnyLen,
+
+		"mixHash":   regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, common.HashLength*2)),
+		"logsBloom": regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, types.BloomByteLength*2)),
+
+		"number":     reHexAnyLen,
+		"difficulty": reHexAnyLen,
+		"gasLimit":   reHexAnyLen,
+		"gasUsed":    reHexAnyLen,
+		"size":       reHexAnyLen,
+		"timestamp":  reHexAnyLen,
+		"extraData":  reHexAnyLen,
+
+		"parentHash":       reHexHashLen,
+		"transactionsRoot": reHexHashLen,
+		"stateRoot":        reHexHashLen,
+		"receiptsRoot":     reHexHashLen,
+		"sha3Uncles":       reHexHashLen,
+
+		"uncles":       regexp.MustCompile(`^\[\]$`),
+		"transactions": regexp.MustCompile(`^\[\]$`),
+	}
+
+	// Construct the 'pending' block expectations as a copy of the concrete block
+	// expectations.
+	pendingBlockExpectations := map[string]*regexp.Regexp{}
+	for k, v := range completeBlockExpectations {
+		pendingBlockExpectations[k] = v
+	}
+
+	// Make 'pending' specific adjustments.
+	pendingBlockExpectations["nonce"] = reNull
+	pendingBlockExpectations["hash"] = reNull
+	pendingBlockExpectations["miner"] = reNull
+	pendingBlockExpectations["totalDifficulty"] = reNull
+
 	for blockHeight, cases := range map[string]map[string]*regexp.Regexp{
-		"earliest": {
-			"nonce": reHexAnyLen,
-			"hash":  reHexAnyLen,
-			"miner": regexp.MustCompile(fmt.Sprintf(`^"0x[a-zA-Z0-9]{%d}"$`, common.AddressLength*2)),
-
-			"totalDifficulty": reHexAnyLen,
-
-			"mixHash":   regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, common.HashLength*2)),
-			"logsBloom": regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, types.BloomByteLength*2)),
-
-			"number":     reHexAnyLen,
-			"difficulty": reHexAnyLen,
-			"gasLimit":   reHexAnyLen,
-			"gasUsed":    reHexAnyLen,
-			"size":       reHexAnyLen,
-			"timestamp":  reHexAnyLen,
-			"extraData":  reHexAnyLen,
-
-			"parentHash":       reHexHashLen,
-			"transactionsRoot": reHexHashLen,
-			"stateRoot":        reHexHashLen,
-			"receiptsRoot":     reHexHashLen,
-			"sha3Uncles":       reHexHashLen,
-
-			"uncles":       regexp.MustCompile(`^\[\]$`),
-			"transactions": regexp.MustCompile(`^\[\]$`),
-		},
-		"latest": {
-			"nonce": reHexAnyLen,
-			"hash":  reHexAnyLen,
-			"miner": regexp.MustCompile(fmt.Sprintf(`^"0x[a-zA-Z0-9]{%d}"$`, common.AddressLength*2)),
-
-			"totalDifficulty": reHexAnyLen,
-
-			"mixHash":   regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, common.HashLength*2)),
-			"logsBloom": regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, types.BloomByteLength*2)),
-
-			"number":     reHexAnyLen,
-			"difficulty": reHexAnyLen,
-			"gasLimit":   reHexAnyLen,
-			"gasUsed":    reHexAnyLen,
-			"size":       reHexAnyLen,
-			"timestamp":  reHexAnyLen,
-			"extraData":  reHexAnyLen,
-
-			"parentHash":       reHexHashLen,
-			"transactionsRoot": reHexHashLen,
-			"stateRoot":        reHexHashLen,
-			"receiptsRoot":     reHexHashLen,
-			"sha3Uncles":       reHexHashLen,
-
-			"uncles":       regexp.MustCompile(`^\[\]$`),
-			"transactions": regexp.MustCompile(`^\[\]$`),
-		},
-		"pending": {
-			"nonce": reNull,
-			"hash":  reNull,
-			"miner": reNull,
-
-			"totalDifficulty": reNull,
-
-			"mixHash":   regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, common.HashLength*2)),
-			"logsBloom": regexp.MustCompile(fmt.Sprintf(`^"0x[0]{%d}"$`, types.BloomByteLength*2)),
-
-			"number":     reHexAnyLen,
-			"difficulty": reHexAnyLen,
-			"gasLimit":   reHexAnyLen,
-			"gasUsed":    reHexAnyLen,
-			"size":       reHexAnyLen,
-			"timestamp":  reHexAnyLen,
-			"extraData":  reHexAnyLen,
-
-			"parentHash":       reHexHashLen,
-			"transactionsRoot": reHexHashLen,
-			"stateRoot":        reHexHashLen,
-			"receiptsRoot":     reHexHashLen,
-			"sha3Uncles":       reHexHashLen,
-
-			"uncles":       regexp.MustCompile(`^\[\]$`),
-			"transactions": regexp.MustCompile(`^\[\]$`),
-		},
+		"earliest": completeBlockExpectations,
+		"latest":   completeBlockExpectations,
+		"pending":  pendingBlockExpectations,
 	} {
 		for _, fullTxes := range []bool{true, false} {
-			gotPending := make(map[string]json.RawMessage)
-			err := client.CallContext(ctx, &gotPending, "eth_getBlockByNumber", blockHeight, fullTxes)
-			if err != nil {
-				t.Fatal(err)
-			}
+			t.Run(fmt.Sprintf("eth_getBlockByNumber-%s-%v", blockHeight, fullTxes), func(t *testing.T) {
+				gotPending := make(map[string]json.RawMessage)
+				err := client.CallContext(ctx, &gotPending, "eth_getBlockByNumber", blockHeight, fullTxes)
+				if err != nil {
+					t.Fatal(err)
+				}
 
-			for key, re := range cases {
-				gotVal, ok := gotPending[key]
-				if !ok {
-					t.Errorf("%s: missing key", key)
+				for key, re := range cases {
+					gotVal, ok := gotPending[key]
+					if !ok {
+						t.Errorf("%s: missing key", key)
+					}
+					if !re.Match(gotVal) {
+						t.Errorf("%s want: %v, got: %v", key, re, string(gotVal))
+					}
 				}
-				if !re.Match(gotVal) {
-					t.Errorf("%s want: %v, got: %v", key, re, string(gotVal))
-				}
-			}
 
-			for k, v := range gotPending {
-				if _, ok := cases[k]; !ok {
-					t.Errorf("%s: missing key (value: %v)", k, string(v))
+				for k, v := range gotPending {
+					if _, ok := cases[k]; !ok {
+						t.Errorf("%s: missing key (value: %v)", k, string(v))
+					}
 				}
-			}
+			})
 		}
 	}
 }

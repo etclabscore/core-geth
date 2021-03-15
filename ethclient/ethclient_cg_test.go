904066f67c233f45da2a561306bc1b96dcebb806
meows <b5c6@protonmail.com>
2021-03-15 15:25:59 -0500

    ethclient: additional commentary
    Date: 2021-03-15 15:25:59-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- ethclient/ethclient_cg_test.go
+++ ethclient/ethclient_cg_test.go
@@ -14,6 +14,7 @@ import (
 
 /*
 The '_CanCompareGoEthereum' denotes tests that can be run against ethereum/go-ethereum.
+These tests assume reliance on a shared Go code API (both at the private (test helpers) and public-methods levels).
 See .github/workflows/geth_1to1.yml for exemplary testing.
 */
 

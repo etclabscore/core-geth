9c0bd0f456b6209fe4ce4ebe5f06fb0745dd64c6
Chris Ziogas <ziogaschr@gmail.com>
2021-03-23 20:10:11 +0200

    eth/tracers: add back tests for the call_tracer_parity.js

--- eth/tracers/tracers_test.go
+++ eth/tracers/tracers_test.go
@@ -20,6 +20,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"math/big"
 	"path/filepath"
@@ -47,7 +48,7 @@ import (
 /*
 // makeTest generates a callTracer test by running a prestate reassembled and a
 // call trace run, assembling all the gathered information into a test case.
-var makeTest = function(tx, rewind) {
+var makeTest = function(tx, reexec) {
   // Generate the genesis block from the block, transaction and prestate data
   var block   = eth.getBlock(eth.getTransaction(tx).blockHash);
   var genesis = eth.getBlock(block.parentHash);
@@ -66,14 +67,14 @@ var makeTest = function(tx, rewind) {
   genesis.number    = genesis.number.toString();
   genesis.timestamp = genesis.timestamp.toString();
 
-  genesis.alloc = debug.traceTransaction(tx, {tracer: "prestateTracer", rewind: rewind});
+  genesis.alloc = debug.traceTransaction(tx, {tracer: "prestateTracer", reexec: reexec});
   for (var key in genesis.alloc) {
     genesis.alloc[key].nonce = genesis.alloc[key].nonce.toString();
   }
   genesis.config = admin.nodeInfo.protocols.eth.config;
 
   // Generate the call trace and produce the test input
-  var result = debug.traceTransaction(tx, {tracer: "callTracer", rewind: rewind});
+  var result = debug.traceTransaction(tx, {tracer: "callTracer", reexec: reexec});
   delete result.time;
 
   console.log(JSON.stringify({
@@ -119,6 +120,52 @@ type callTracerTest struct {
 	Context *callContext      `json:"context"`
 	Input   string            `json:"input"`
 	Result  *callTrace        `json:"result"`
+}
+
+// callTraceParity is the result of a callTracerParity run.
+type callTraceParity struct {
+	Action              callTraceParityAction `json:"action"`
+	BlockHash           *common.Hash          `json:"-"`
+	BlockNumber         uint64                `json:"-"`
+	Error               string                `json:"error,omitempty"`
+	Result              callTraceParityResult `json:"result"`
+	Subtraces           int                   `json:"subtraces"`
+	TraceAddress        []int                 `json:"traceAddress"`
+	TransactionHash     *common.Hash          `json:"-"`
+	TransactionPosition *uint64               `json:"-"`
+	Type                string                `json:"type"`
+	Time                string                `json:"-"`
+}
+
+type callTraceParityAction struct {
+	Author         *common.Address `json:"author,omitempty"`
+	RewardType     *string         `json:"rewardType,omitempty"`
+	SelfDestructed *common.Address `json:"address,omitempty"`
+	Balance        *hexutil.Big    `json:"balance,omitempty"`
+	CallType       string          `json:"callType,omitempty"`
+	CreationMethod string          `json:"creationMethod,omitempty"`
+	From           common.Address  `json:"from,omitempty"`
+	Gas            hexutil.Uint64  `json:"gas,omitempty"`
+	Init           *hexutil.Bytes  `json:"init,omitempty"`
+	Input          *hexutil.Bytes  `json:"input,omitempty"`
+	RefundAddress  *common.Address `json:"refundAddress,omitempty"`
+	To             common.Address  `json:"to,omitempty"`
+	Value          hexutil.Big     `json:"value,omitempty"`
+}
+
+type callTraceParityResult struct {
+	Address *common.Address `json:"address,omitempty"`
+	Code    *hexutil.Bytes  `json:"code,omitempty"`
+	GasUsed hexutil.Uint64  `json:"gasUsed,omitempty"`
+	Output  hexutil.Bytes   `json:"output,omitempty"`
+}
+
+// callTracerParityTest defines a single test to check the call tracer against.
+type callTracerParityTest struct {
+	Genesis *genesisT.Genesis  `json:"genesis"`
+	Context *callContext       `json:"context"`
+	Input   string             `json:"input"`
+	Result  *[]callTraceParity `json:"result"`
 }
 
 func TestPrestateTracerCreate2(t *testing.T) {
@@ -300,4 +347,133 @@ func jsonEqual(x, y interface{}) bool {
 		return false
 	}
 	return reflect.DeepEqual(xTrace, yTrace)
+}
+
+func callTracerParityTestRunner(filename string) error {
+	// Call tracer test found, read if from disk
+	blob, err := ioutil.ReadFile(filepath.Join("testdata", filename))
+	if err != nil {
+		return fmt.Errorf("failed to read testcase: %v", err)
+	}
+	test := new(callTracerParityTest)
+	if err := json.Unmarshal(blob, test); err != nil {
+		return fmt.Errorf("failed to parse testcase: %v", err)
+	}
+	// Configure a blockchain with the given prestate
+	tx := new(types.Transaction)
+	if err := rlp.DecodeBytes(common.FromHex(test.Input), tx); err != nil {
+		return fmt.Errorf("failed to parse testcase input: %v", err)
+	}
+	signer := types.MakeSigner(test.Genesis.Config, new(big.Int).SetUint64(uint64(test.Context.Number)))
+	origin, _ := signer.Sender(tx)
+	txContext := vm.TxContext{
+		Origin:   origin,
+		GasPrice: tx.GasPrice(),
+	}
+	context := vm.BlockContext{
+		CanTransfer: core.CanTransfer,
+		Transfer:    core.Transfer,
+		Coinbase:    test.Context.Miner,
+		BlockNumber: new(big.Int).SetUint64(uint64(test.Context.Number)),
+		Time:        new(big.Int).SetUint64(uint64(test.Context.Time)),
+		Difficulty:  (*big.Int)(test.Context.Difficulty),
+		GasLimit:    uint64(test.Context.GasLimit),
+	}
+	_, statedb := tests.MakePreState(rawdb.NewMemoryDatabase(), test.Genesis.Alloc, false)
+
+	// Create the tracer, the EVM environment and run it
+	tracer, err := New("callTracerParity", txContext)
+	if err != nil {
+		return fmt.Errorf("failed to create call tracer: %v", err)
+	}
+	evm := vm.NewEVM(context, txContext, statedb, test.Genesis.Config, vm.Config{Debug: true, Tracer: tracer})
+
+	msg, err := tx.AsMessage(signer)
+	if err != nil {
+		return fmt.Errorf("failed to prepare transaction for tracing: %v", err)
+	}
+	st := core.NewStateTransition(evm, msg, new(core.GasPool).AddGas(tx.Gas()))
+
+	if _, err = st.TransitionDb(); err != nil {
+		return fmt.Errorf("failed to execute transaction: %v", err)
+	}
+
+	// Retrieve the trace result and compare against the etalon
+	res, err := tracer.GetResult()
+	if err != nil {
+		return fmt.Errorf("failed to retrieve trace result: %v", err)
+	}
+	ret := new([]callTraceParity)
+	if err := json.Unmarshal(res, ret); err != nil {
+		return fmt.Errorf("failed to unmarshal trace result: %v", err)
+	}
+
+	if !jsonEqualParity(ret, test.Result) {
+		// uncomment this for easier debugging
+		// have, _ := json.MarshalIndent(ret, "", " ")
+		// want, _ := json.MarshalIndent(test.Result, "", " ")
+		// return fmt.Errorf("trace mismatch: \nhave %+v\nwant %+v", string(have), string(want))
+		return fmt.Errorf("trace mismatch: \nhave %+v\nwant %+v", ret, test.Result)
+	}
+	return nil
+}
+
+// Iterates over all the input-output datasets in the tracer parity test harness and
+// runs the JavaScript tracers against them.
+func TestCallTracerParity(t *testing.T) {
+	files, err := ioutil.ReadDir("testdata")
+	if err != nil {
+		t.Fatalf("failed to retrieve tracer test suite: %v", err)
+	}
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), "parity_call_tracer_") {
+			continue
+		}
+		file := file // capture range variable
+		t.Run(camel(strings.TrimSuffix(strings.TrimPrefix(file.Name(), "parity_call_tracer_"), ".json")), func(t *testing.T) {
+			t.Parallel()
+
+			err := callTracerParityTestRunner(file.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+// jsonEqual is similar to reflect.DeepEqual, but does a 'bounce' via json prior to
+// comparison
+func jsonEqualParity(x, y interface{}) bool {
+	xTrace := new([]callTraceParity)
+	yTrace := new([]callTraceParity)
+	if xj, err := json.Marshal(x); err == nil {
+		json.Unmarshal(xj, xTrace)
+	} else {
+		return false
+	}
+	if yj, err := json.Marshal(y); err == nil {
+		json.Unmarshal(yj, yTrace)
+	} else {
+		return false
+	}
+	return reflect.DeepEqual(xTrace, yTrace)
+}
+
+func BenchmarkCallTracerParity(b *testing.B) {
+	files, err := filepath.Glob("testdata/parity_call_tracer_*.json")
+	if err != nil {
+		b.Fatalf("failed to read testdata: %v", err)
+	}
+
+	for _, file := range files {
+		filename := strings.TrimPrefix(file, "testdata/")
+		b.Run(camel(strings.TrimSuffix(strings.TrimPrefix(filename, "parity_call_tracer_"), ".json")), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				err := callTracerParityTestRunner(filename)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
 }

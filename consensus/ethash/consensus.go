a80c756137a8abb937a6e359f869fb183ffeb01f
meows <b5c6@protonmail.com>
2021-03-22 19:14:31 -0500

    consensus/ethash: allowedFutureBlockTime mismatched types
    Date: 2021-03-22 19:14:31-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -230,7 +230,7 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainHeaderReader, header, pa
 	}
 	// Verify the header's timestamp
 	if !uncle {
-		if header.Time > uint64(unixNow+allowedFutureBlockTimeSeconds) {
+		if header.Time > uint64(unixNow+int64(allowedFutureBlockTime.Seconds())) {
 			return consensus.ErrFutureBlock
 		}
 	}

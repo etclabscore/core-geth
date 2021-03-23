bdce3fc7058145ffa103a83236de6acaf3257840
meows <b5c6@protonmail.com>
2021-03-23 07:18:25 -0500

    eth: [INVOLVED/REVIEWME] undeclared name: newTestProtocolManagerMust
    Date: 2021-03-23 07:18:25-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- eth/sync_test.go
+++ eth/sync_test.go
@@ -17,16 +17,21 @@
 package eth
 
 import (
+	"math/big"
 	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/eth/protocols/eth"
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-ethereum/params/types/genesisT"
 )
 
 // blockGenContemporaryTime creates a block gen function that will bump the block times to within throwing
@@ -42,6 +47,43 @@ func blockGenContemporaryTime(numBlocks int64) func(i int, gen *core.BlockGen) {
 			// 10 is the magic number that the test helper block gen will use for the block time offsets.
 			gen.OffsetTime(startTimeUnix - (numBlocks * 10))
 		}
+	}
+}
+
+// newTestHandlerWithBlocks creates a new handler for testing purposes, with a
+// given number of initial blocks.
+func newTestHandlerWithBlocksWithOpts(blocks int, mode downloader.SyncMode, gen func(int, *core.BlockGen)) *testHandler {
+	// Create a database pre-initialize with a genesis block
+	db := rawdb.NewMemoryDatabase()
+	gspec := &genesisT.Genesis{
+		Config: params.TestChainConfig,
+		Alloc:  genesisT.GenesisAlloc{testAddr: {Balance: big.NewInt(1000000)}},
+	}
+	core.MustCommitGenesis(db, gspec)
+
+	chain, _ := core.NewBlockChain(db, nil, params.TestChainConfig, ethash.NewFaker(), vm.Config{}, nil, nil)
+
+	bs, _ := core.GenerateChain(params.TestChainConfig, chain.Genesis(), ethash.NewFaker(), db, blocks, gen)
+	if _, err := chain.InsertChain(bs); err != nil {
+		panic(err)
+	}
+	txpool := newTestTxPool()
+
+	handler, _ := newHandler(&handlerConfig{
+		Database:   db,
+		Chain:      chain,
+		TxPool:     txpool,
+		Network:    1,
+		Sync:       mode,
+		BloomCache: 1,
+	})
+	handler.Start(1000)
+
+	return &testHandler{
+		db:      db,
+		chain:   chain,
+		txpool:  txpool,
+		handler: handler,
 	}
 }
 
@@ -102,48 +144,64 @@ func TestArtificialFinalityFeatureEnablingDisabling(t *testing.T) {
 	genFunc := blockGenContemporaryTime(int64(maxBlocksCreated))
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	a, _ := newTestProtocolManagerMust(t, downloader.FastSync, maxBlocksCreated, genFunc, nil)
-	if atomic.LoadUint32(&a.fastSync) == 1 {
+	a := newTestHandlerWithBlocksWithOpts(1024, downloader.FullSync, genFunc)
+	if atomic.LoadUint32(&a.handler.fastSync) == 1 {
 		t.Fatalf("fast sync not disabled on non-empty blockchain")
 	}
+	defer a.close()
 
 	one := uint64(1)
-	a.blockchain.Config().SetECBP1100Transition(&one)
+	a.chain.Config().SetECBP1100Transition(&one)
 
 	oMinAFPeers := minArtificialFinalityPeers
 	defer func() {
-		// Clean up after, resetting global default to original valu.
+		// Clean up after, resetting global default to original value.
 		minArtificialFinalityPeers = oMinAFPeers
 	}()
 	minArtificialFinalityPeers = 1
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	b, _ := newTestProtocolManagerMust(t, downloader.FastSync, 0, genFunc, nil)
-	if atomic.LoadUint32(&b.fastSync) == 0 {
-		t.Fatalf("fast sync disabled on pristine blockchain")
+	b := newTestHandlerWithBlocksWithOpts(0, downloader.FullSync, genFunc)
+	if atomic.LoadUint32(&b.handler.fastSync) == 1 {
+		t.Fatalf("fast sync not disabled on non-empty blockchain")
 	}
-	b.blockchain.Config().SetECBP1100Transition(&one)
+	defer b.close()
+	b.chain.Config().SetECBP1100Transition(&one)
 	// b.chainSync.forced = true
 
-	io1, io2 := p2p.MsgPipe()
-	go a.handle(a.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-b", nil), io2, a.txpool.Get))
-	go b.handle(b.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-a", nil), io1, b.txpool.Get))
+	// Sync up the two handlers
+	emptyPipe, fullPipe := p2p.MsgPipe()
+	defer emptyPipe.Close()
+	defer fullPipe.Close()
+
+	fullPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{2}, "", nil), fullPipe, a.txpool)
+	emptyPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{1}, "", nil), emptyPipe, b.txpool)
+	defer emptyPeer.Close()
+	defer fullPeer.Close()
+
+	go b.handler.runEthPeer(emptyPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(b.handler), peer)
+	})
+	go a.handler.runEthPeer(fullPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(a.handler), peer)
+	})
+	// Wait a bit for the above handlers to start
 	time.Sleep(250 * time.Millisecond)
 
-	op := peerToSyncOp(downloader.FullSync, b.peers.BestPeer())
-	if err := b.doSync(op); err != nil {
+	op := peerToSyncOp(downloader.FullSync, b.handler.peers.peerWithHighestTD())
+	if err := b.handler.doSync(op); err != nil {
 		t.Fatalf("sync failed: %v", err)
 	}
 
-	b.chainSync.forced = true
-	next := b.chainSync.nextSyncOp()
+	b.handler.chainSync.forced = true
+	next := b.handler.chainSync.nextSyncOp()
 	if next != nil {
 		t.Fatal("non-nil next sync op")
 	}
-	if !b.blockchain.Config().IsEnabled(b.blockchain.Config().GetECBP1100Transition, b.blockchain.CurrentBlock().Number()) {
+	if !b.chain.Config().IsEnabled(b.chain.Config().GetECBP1100Transition, b.chain.CurrentBlock().Number()) {
 		t.Error("AF feature not configured")
 	}
-	if !b.blockchain.IsArtificialFinalityEnabled() {
+	if !b.chain.IsArtificialFinalityEnabled() {
 		t.Error("AF not enabled")
 	}
 
@@ -151,12 +209,12 @@ func TestArtificialFinalityFeatureEnablingDisabling(t *testing.T) {
 	minArtificialFinalityPeers = oMinAFPeers
 
 	// Next sync op will unset AF because manager only has 1 peer.
-	b.chainSync.forced = true
-	next = b.chainSync.nextSyncOp()
+	b.handler.chainSync.forced = true
+	next = b.handler.chainSync.nextSyncOp()
 	if next != nil {
 		t.Fatal("non-nil next sync op")
 	}
-	if b.blockchain.IsArtificialFinalityEnabled() {
+	if b.chain.IsArtificialFinalityEnabled() {
 		t.Error("AF not disabled")
 	}
 }
@@ -168,13 +226,11 @@ func TestArtificialFinalityFeatureEnablingDisabling_NoDisable(t *testing.T) {
 	genFunc := blockGenContemporaryTime(int64(maxBlocksCreated))
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	a, _ := newTestProtocolManagerMust(t, downloader.FastSync, maxBlocksCreated, genFunc, nil)
-	if atomic.LoadUint32(&a.fastSync) == 1 {
-		t.Fatalf("fast sync not disabled on non-empty blockchain")
-	}
+	a := newTestHandlerWithBlocksWithOpts(1024, downloader.FullSync, genFunc)
+	defer a.close()
 
 	one := uint64(1)
-	a.blockchain.Config().SetECBP1100Transition(&one)
+	a.chain.Config().SetECBP1100Transition(&one)
 
 	oMinAFPeers := minArtificialFinalityPeers
 	defer func() {
@@ -184,29 +240,41 @@ func TestArtificialFinalityFeatureEnablingDisabling_NoDisable(t *testing.T) {
 	minArtificialFinalityPeers = 1
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	b, _ := newTestProtocolManagerMust(t, downloader.FastSync, 0, genFunc, nil)
-	if atomic.LoadUint32(&b.fastSync) == 0 {
-		t.Fatalf("fast sync disabled on pristine blockchain")
-	}
-	b.blockchain.Config().SetECBP1100Transition(&one)
+	b := newTestHandlerWithBlocksWithOpts(0, downloader.FullSync, genFunc)
+	defer b.close()
+	b.chain.Config().SetECBP1100Transition(&one)
 
 	// NOTE1: Set the nodisable switch to the on position.
 	// This prevents the de-activation of AF features once they have been enabled.
 	// In geth code, this is set in the backend during blockchain construction.
-	b.blockchain.ArtificialFinalityNoDisable(1)
+	b.chain.ArtificialFinalityNoDisable(1)
 
-	io1, io2 := p2p.MsgPipe()
-	go a.handle(a.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-b", nil), io2, a.txpool.Get))
-	go b.handle(b.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-a", nil), io1, b.txpool.Get))
+	// Sync up the two handlers
+	emptyPipe, fullPipe := p2p.MsgPipe()
+	defer emptyPipe.Close()
+	defer fullPipe.Close()
+
+	fullPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{2}, "", nil), fullPipe, a.txpool)
+	emptyPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{1}, "", nil), emptyPipe, b.txpool)
+	defer emptyPeer.Close()
+	defer fullPeer.Close()
+
+	go b.handler.runEthPeer(emptyPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(b.handler), peer)
+	})
+	go a.handler.runEthPeer(fullPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(a.handler), peer)
+	})
+	// Wait a bit for the above handlers to start
 	time.Sleep(250 * time.Millisecond)
 
-	op := peerToSyncOp(downloader.FullSync, b.peers.BestPeer())
-	if err := b.doSync(op); err != nil {
+	op := peerToSyncOp(downloader.FullSync, b.handler.peers.peerWithHighestTD())
+	if err := b.handler.doSync(op); err != nil {
 		t.Fatalf("sync failed: %v", err)
 	}
 
-	b.chainSync.forced = true
-	next := b.chainSync.nextSyncOp()
+	b.handler.chainSync.forced = true
+	next := b.handler.chainSync.nextSyncOp()
 
 	// Revert safety condition overrides to default values.
 	// Set the value back to default (more than 1).
@@ -215,20 +283,20 @@ func TestArtificialFinalityFeatureEnablingDisabling_NoDisable(t *testing.T) {
 	if next != nil {
 		t.Fatal("non-nil next sync op")
 	}
-	if !b.blockchain.Config().IsEnabled(b.blockchain.Config().GetECBP1100Transition, b.blockchain.CurrentBlock().Number()) {
+	if !b.chain.Config().IsEnabled(b.chain.Config().GetECBP1100Transition, b.chain.CurrentBlock().Number()) {
 		t.Error("AF feature not configured")
 	}
-	if !b.blockchain.IsArtificialFinalityEnabled() {
+	if !b.chain.IsArtificialFinalityEnabled() {
 		t.Error("AF not enabled")
 	}
 
 	// Next sync op will unset AF because manager only has 1 peer.
-	b.chainSync.forced = true
-	next = b.chainSync.nextSyncOp()
+	b.handler.chainSync.forced = true
+	next = b.handler.chainSync.nextSyncOp()
 	if next != nil {
 		t.Fatal("non-nil next sync op")
 	}
-	if !b.blockchain.IsArtificialFinalityEnabled() {
+	if !b.chain.IsArtificialFinalityEnabled() {
 		t.Error(`AF not enabled;
 The AF disable mechanism triggered by the minimum peers floor should have been short-circuited,
 preventing AF disablement on this sync op (with the minArtificialFinalityPeers value set to > 1 (defaultMinSyncPeers = 5)),
@@ -244,13 +312,10 @@ func TestArtificialFinalityFeatureEnablingDisabling_StaleHead(t *testing.T) {
 	maxBlocksCreated := 1024
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	a, _ := newTestProtocolManagerMust(t, downloader.FastSync, maxBlocksCreated, nil, nil)
-	if atomic.LoadUint32(&a.fastSync) == 1 {
-		t.Fatalf("fast sync not disabled on non-empty blockchain")
-	}
-
+	a := newTestHandlerWithBlocksWithOpts(maxBlocksCreated, downloader.FullSync, nil)
+	defer a.close()
 	one := uint64(1)
-	a.blockchain.Config().SetECBP1100Transition(&one)
+	a.chain.Config().SetECBP1100Transition(&one)
 
 	oMinAFPeers := minArtificialFinalityPeers
 	defer func() {
@@ -260,24 +325,36 @@ func TestArtificialFinalityFeatureEnablingDisabling_StaleHead(t *testing.T) {
 	minArtificialFinalityPeers = 1
 
 	// Create a full protocol manager, check that fast sync gets disabled
-	b, _ := newTestProtocolManagerMust(t, downloader.FastSync, 0, nil, nil)
-	if atomic.LoadUint32(&b.fastSync) == 0 {
-		t.Fatalf("fast sync disabled on pristine blockchain")
-	}
-	b.blockchain.Config().SetECBP1100Transition(&one)
+	b := newTestHandlerWithBlocksWithOpts(0, downloader.FullSync, nil)
+	defer b.close()
+	b.chain.Config().SetECBP1100Transition(&one)
 
-	io1, io2 := p2p.MsgPipe()
-	go a.handle(a.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-b", nil), io2, a.txpool.Get))
-	go b.handle(b.newPeer(65, p2p.NewPeer(enode.ID{}, "peer-a", nil), io1, b.txpool.Get))
+	// Sync up the two handlers
+	emptyPipe, fullPipe := p2p.MsgPipe()
+	defer emptyPipe.Close()
+	defer fullPipe.Close()
+
+	fullPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{2}, "", nil), fullPipe, a.txpool)
+	emptyPeer := eth.NewPeer(65, p2p.NewPeer(enode.ID{1}, "", nil), emptyPipe, b.txpool)
+	defer emptyPeer.Close()
+	defer fullPeer.Close()
+
+	go b.handler.runEthPeer(emptyPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(b.handler), peer)
+	})
+	go a.handler.runEthPeer(fullPeer, func(peer *eth.Peer) error {
+		return eth.Handle((*ethHandler)(a.handler), peer)
+	})
+	// Wait a bit for the above handlers to start
 	time.Sleep(250 * time.Millisecond)
 
-	op := peerToSyncOp(downloader.FullSync, b.peers.BestPeer())
-	if err := b.doSync(op); err != nil {
+	op := peerToSyncOp(downloader.FullSync, b.handler.peers.peerWithHighestTD())
+	if err := b.handler.doSync(op); err != nil {
 		t.Fatalf("sync failed: %v", err)
 	}
 
-	b.chainSync.forced = true
-	next := b.chainSync.nextSyncOp()
+	b.handler.chainSync.forced = true
+	next := b.handler.chainSync.nextSyncOp()
 
 	// Revert safety condition overrides to default values.
 	// Set the value back to default (more than 1).
@@ -286,16 +363,16 @@ func TestArtificialFinalityFeatureEnablingDisabling_StaleHead(t *testing.T) {
 	if next != nil {
 		t.Fatal("non-nil next sync op")
 	}
-	if !b.blockchain.Config().IsEnabled(b.blockchain.Config().GetECBP1100Transition, b.blockchain.CurrentBlock().Number()) {
+	if !b.chain.Config().IsEnabled(b.chain.Config().GetECBP1100Transition, b.chain.CurrentBlock().Number()) {
 		t.Error("AF feature not configured")
 	}
 	// Unit test the timestamp. We want to be sure that the blockchain's current header is actually very old
 	// (since we expect its age to act as a condition preventing the enabling of AF features).
-	d := uint64(time.Now().Unix()) - b.blockchain.CurrentHeader().Time
+	d := uint64(time.Now().Unix()) - b.chain.CurrentHeader().Time
 	if time.Second*time.Duration(d) < artificialFinalitySafetyInterval {
 		t.Errorf("Expected blockchain current head to be very old, it is not.")
 	}
-	if b.blockchain.IsArtificialFinalityEnabled() {
+	if b.chain.IsArtificialFinalityEnabled() {
 		t.Error("AF is enabled despite a very stale head")
 	}
 }

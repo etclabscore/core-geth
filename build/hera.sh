f4a3ec44e313f26cce60fbf596d7751c2e82e263
Chris Ziogas <ziogaschr@gmail.com>
2021-03-29 13:50:23 +0300

    build: Exit with error on non linux envs during evmc-v7 builds

--- build/hera.sh
+++ build/hera.sh
@@ -7,6 +7,11 @@ set -e
 # wget -P build/_workspace/hera/ https://github.com/ewasm/hera/releases/download/v0.3.2/hera-0.3.2-linux-x86_64.tar.gz
 # tar -C build/_workspace/hera/0.3.2 -xzf build/_workspace/hera/hera-0.3.2-linux-x86_64.tar.gz
 
+if [[ "$OSTYPE" != "linux"* ]]; then
+    echo "This script is only currently configured to work on Linux. Please see \"https://github.com/ewasm/hera#building-hera\" documentation for instructions to build in other environments."
+    exit 1
+fi
+
 main() {
     mkdir -p build/_workspace
     [ ! -d build/_workspace/hera ] && git clone https://github.com/ewasm/hera build/_workspace/hera || echo "Hera exists."
--- build/ssvm.sh
+++ build/ssvm.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ "$OSTYPE" != "linux"* ]]; then
+    echo "This script is only currently configured to work on Linux. Please see \"https://github.com/second-state/ssvm-evmc#notice\" documentation for instructions to build in other environments."
+    exit 1
+fi
+
 mkdir -p build/_workspace/SSVM/build/tools/ssvm-evmc/
 wget -O build/_workspace/SSVM/build/tools/ssvm-evmc/libssvmEVMC.so \
     https://github.com/second-state/ssvm-evmc/releases/download/evmc7-0.1.1/libssvm-evmc.so

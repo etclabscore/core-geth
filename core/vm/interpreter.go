53eaf176a13ce33f4351734430720ae2af4f2123
Chris Ziogas <ziogaschr@gmail.com>
2021-03-31 16:17:33 +0300

    core/vm: revert prettyfication commit
    It reassigns the `err` var in the if context instead of changing the outer context for the function return

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -254,7 +254,8 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		// consume the gas and return an error if not enough gas is available.
 		// cost is explicitly set so that the capture state defer method can get the proper cost
 		if operation.dynamicGas != nil {
-			dynamicCost, err := operation.dynamicGas(in.evm, contract, stack, mem, memorySize)
+			var dynamicCost uint64
+			dynamicCost, err = operation.dynamicGas(in.evm, contract, stack, mem, memorySize)
 			cost += dynamicCost // total cost, for debug tracing
 			if err != nil || !contract.UseGas(dynamicCost) {
 				return nil, ErrOutOfGas

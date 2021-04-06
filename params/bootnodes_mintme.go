9d5b9e30014cac0f2975014426e8cb34f1fcc21a
lumat <lukasz1235@gmail.com>
2021-04-06 15:36:27 +0200

    [MINTME][WIP] Add Mintme.com Coin params

---
+++ params/bootnodes_mintme.go
@@ -0,0 +1,12 @@
+package params
+
+// MINTMEBootnodes are the enode URLs of the P2P bootstrap nodes running
+// reliably and availably on the MINTME network.
+var MINTMEBootnodes = []string{
+    //"enode://3e12c4c633157ae52e7e05c168f4b1aa91685a36ba33a0901aa8a83cfeb84c3633226e3dd2eaf59bfc83492139e1d68918bf5b60ba93e2deaedb4e6a2ded5d32@42.152.120.98:30303",
+    // TODO: lumat
+}
+
+// Once MINTME network has DNS discovery set up,
+// this value can be configured.
+// var MINTMEDNSNetwork = "enrtree://AJE62Q4DUX4QMMXEHCSSCSC65TDHZYSMONSD64P3WULVLSF6MRQ3K@example.network"

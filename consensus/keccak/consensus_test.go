17e1a6e94abc9d064bf84e6c34eb60c696554d0c
meows <b5c6@protonmail.com>
2021-08-04 12:13:48 -0500

    consensus/keccak: remove failing test specific to Ethash
    Date: 2021-08-04 12:13:48-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- consensus/keccak/consensus_test.go
+++ consensus/keccak/consensus_test.go
@@ -19,13 +19,8 @@ package keccak
 import (
 	"encoding/json"
 	"math/big"
-	"os"
-	"path/filepath"
-	"testing"
 
 	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/params/types/goethereum"
 )
 
 type diffTest struct {
@@ -55,32 +50,4 @@ func (d *diffTest) UnmarshalJSON(b []byte) (err error) {
 	d.CurrentDifficulty = math.MustParseBig256(ext.CurrentDifficulty)
 
 	return nil
-}
-
-func TestCalcDifficulty(t *testing.T) {
-	file, err := os.Open(filepath.Join("..", "..", "tests", "testdata", "BasicTests", "difficulty.json"))
-	if err != nil {
-		t.Skip(err)
-	}
-	defer file.Close()
-
-	tests := make(map[string]diffTest)
-	err = json.NewDecoder(file).Decode(&tests)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	config := &goethereum.ChainConfig{HomesteadBlock: big.NewInt(1150000)}
-
-	for name, test := range tests {
-		number := new(big.Int).Sub(test.CurrentBlocknumber, big.NewInt(1))
-		diff := CalcDifficulty(config, test.CurrentTimestamp, &types.Header{
-			Number:     number,
-			Time:       test.ParentTimestamp,
-			Difficulty: test.ParentDifficulty,
-		})
-		if diff.Cmp(test.CurrentDifficulty) != 0 {
-			t.Error(name, "failed. Expected", test.CurrentDifficulty, "and calculated", diff)
-		}
-	}
 }

a31a7c125d9693178dbdf741cc65fd77c8d4d543
meows <b5c6@protonmail.com>
2021-08-25 14:52:35 -0500

    core: fix failing TestStateProcessorErrors
    This test failed because the expected error strings
    were not exactly the error strings that were getting
    returned. This adjusts the error message returned when
    the buyGas function fails with insufficient funds.
    
    Date: 2021-08-25 14:52:35-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/state_transition.go
+++ core/state_transition.go
@@ -199,7 +199,8 @@ func (st *StateTransition) buyGas() error {
 		balanceCheck.Add(balanceCheck, st.value)
 	}
 	if !st.evm.Context.CanTransfer(st.state, st.msg.From(), balanceCheck) {
-		return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), st.state, st.msg.From(), balanceCheck)
+		have, want := st.state.GetBalance(st.msg.From()), balanceCheck
+		return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
 	}
 	if err := st.gp.SubGas(st.msg.Gas()); err != nil {
 		return err

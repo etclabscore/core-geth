e105ba5422067b9f4a2c14a8ed429dfc840be3a9
Chris Ziogas <ziogaschr@gmail.com>
2021-10-26 19:38:59 +0300

    Move release artifacts build from Travis to GitHub actions

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-GETH_ARCHIVE_NAME="core-geth-${TRAVIS_OS_NAME}-$(git describe --always)"
-zip -j "$GETH_ARCHIVE_NAME.zip" build/bin/geth
 
-shasum -a 256 $GETH_ARCHIVE_NAME.zip
-shasum -a 256 $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
+GETH_ARCHIVE_NAME="core-geth-${BUILD_OS_NAME}"
+ALLTOOLS_ARCHIVE_NAME="core-geth-alltools-${BUILD_OS_NAME}"
 
-ALLTOOLS_ARCHIVE_NAME="core-geth-alltools-${TRAVIS_OS_NAME}-$(git describe --always)"
-zip -j "$ALLTOOLS_ARCHIVE_NAME.zip" build/bin/*
+if [[ "${BUILD_OS_NAME}" == "win64" ]]; then
+  7z a "$GETH_ARCHIVE_NAME.zip" ./build/bin/geth.exe
 
-shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip
-shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip > $ALLTOOLS_ARCHIVE_NAME.zip.sha256
+  sha256sum $GETH_ARCHIVE_NAME.zip
+  sha256sum $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
 
+  7z a "$ALLTOOLS_ARCHIVE_NAME.zip" ./build/bin/*
+
+  sha256sum $ALLTOOLS_ARCHIVE_NAME.zip
+  sha256sum $ALLTOOLS_ARCHIVE_NAME.zip > $ALLTOOLS_ARCHIVE_NAME.zip.sha256
+
+else
+  zip -j "$GETH_ARCHIVE_NAME.zip" build/bin/geth
+
+  shasum -a 256 $GETH_ARCHIVE_NAME.zip
+  shasum -a 256 $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
+
+  zip -j "$ALLTOOLS_ARCHIVE_NAME.zip" build/bin/*
+
+  shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip
+  shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip > $ALLTOOLS_ARCHIVE_NAME.zip.sha256
+fi

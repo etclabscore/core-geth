6a8dfc00d6b32ce4fc225ca4df499aaf0c1255fe
meows <b5c6@protonmail.com>
2022-04-05 08:13:23 -0700

    eth: .TerminalTotalDifficulty -> .GetEthashTerminalTotal...
    Date: 2022-04-05 08:13:23-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- eth/sync.go
+++ eth/sync.go
@@ -311,7 +311,7 @@ func (h *handler) doSync(op *chainSyncOp) error {
 		}
 	}
 	// Run the sync cycle, and disable snap sync if we're past the pivot block
-	err := h.downloader.LegacySync(op.peer.ID(), op.head, op.td, h.chain.Config().TerminalTotalDifficulty, op.mode)
+	err := h.downloader.LegacySync(op.peer.ID(), op.head, op.td, h.chain.Config().GetEthashTerminalTotalDifficulty(), op.mode)
 	if err != nil {
 		return err
 	}

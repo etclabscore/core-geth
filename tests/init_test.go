4b1b83cdab285f7902c0872ee43e02f00db28185
meows <b5c6@protonmail.com>
2022-08-17 09:02:52 -0500

    tests: generate difficulty tests
    Date: 2022-08-17 09:02:52-05:00
    Signed-off-by: meows <b5c6@protonmail.com>
    
    remove dead code
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- tests/init_test.go
+++ tests/init_test.go
@@ -65,9 +65,8 @@ var (
 	legacyStateTestDir = filepath.Join(baseDir, "LegacyTests", "Constantinople", "GeneralStateTests")
 	transactionTestDir = filepath.Join(baseDir, "TransactionTests")
 	rlpTestDir         = filepath.Join(baseDir, "RLPTests")
-	// difficultyTestDir  = filepath.Join(baseDir, "BasicTests.core-geth")
-	difficultyTestDir = filepath.Join(baseDir, "BasicTests")
-	benchmarksDir     = filepath.Join(".", "evm-benchmarks", "benchmarks")
+	difficultyTestDir  = filepath.Join(baseDir, "BasicTests")
+	benchmarksDir      = filepath.Join(".", "evm-benchmarks", "benchmarks")
 )
 
 func readJSON(reader io.Reader, value interface{}) error {

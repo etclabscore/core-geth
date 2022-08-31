a4ee164c012a7fe3bb414a8da575fef73e87d127
meows <b5c6@protonmail.com>
2022-08-31 15:21:22 -0700

    internal/ethapi: b.ChainConfig().IsLondon undefined
    Date: 2022-08-31 15:21:22-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- internal/ethapi/transaction_args.go
+++ internal/ethapi/transaction_args.go
@@ -149,7 +149,7 @@ func (args *TransactionArgs) setFeeDefaults(ctx context.Context, b Backend) erro
 	}
 	// Now attempt to fill in default value depending on whether London is active or not.
 	head := b.CurrentHeader()
-	if b.ChainConfig().IsLondon(head.Number) {
+	if b.ChainConfig().IsEnabled(b.ChainConfig().GetEIP1559Transition, head.Number) {
 		// London is active, set maxPriorityFeePerGas and maxFeePerGas.
 		if err := args.setLondonFeeDefaults(ctx, head, b); err != nil {
 			return err

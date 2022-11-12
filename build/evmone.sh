bc5cc3ec773a8126c8a779b3dcfb52c0f2b0aa1d
meows <b5c6@protonmail.com>
2022-11-12 07:43:47 -0800

    build: better support for different versions
    Date: 2022-11-12 07:43:47-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/evmone.sh
+++ build/evmone.sh
@@ -2,12 +2,27 @@
 
 set -e
 
+TARGET_VERSION=0.9.1
+# TARGET_VERSION=0.9.0
+# TARGET_VERSION=0.6.0
+
+
 if [[ "$OSTYPE" != "linux"* ]]; then
     echo "This script is only currently configured to work on Linux. Please see \"https://github.com/ethereum/evmone\" documentation for instructions to build in other environments."
     exit 1
 fi
 
+download()
+{
+    wget -O build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz https://github.com/ethereum/evmone/releases/download/v${TARGET_VERSION}/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz
+}
+
+unpack()
+{
+    tar xzvf build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz -C build/_workspace/evmone/
+}
+
 mkdir -p build/_workspace/evmone
-[[ -f build/_workspace/evmone/evmone-0.9.1-linux-x86_64.tar.gz ]] && exit 0
-wget -O build/_workspace/evmone/evmone-0.9.1-linux-x86_64.tar.gz https://github.com/ethereum/evmone/releases/download/v0.9.1/evmone-0.9.1-linux-x86_64.tar.gz
-tar xzvf build/_workspace/evmone/evmone-0.9.1-linux-x86_64.tar.gz -C build/_workspace/evmone/
+[[ -f build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz ]] || download
+unpack
+

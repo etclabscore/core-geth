e468081ec17f5fb5c9327fb1715474172a6a833c
meows <b5c6@protonmail.com>
2022-11-14 10:18:46 -0800

    build: add darwin support for evmone script
    Date: 2022-11-14 10:18:46-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- build/evmone.sh
+++ build/evmone.sh
@@ -6,15 +6,19 @@ TARGET_VERSION=0.9.1
 # TARGET_VERSION=0.9.0
 # TARGET_VERSION=0.6.0
 
-
-if [[ "$OSTYPE" != "linux"* ]]; then
-    echo "This script is only currently configured to work on Linux. Please see \"https://github.com/ethereum/evmone\" documentation for instructions to build in other environments."
-    exit 1
+_OSTYPE=${OSTYPE}
+if [[ ${_OSTYPE} == "linux"* ]]; then
+    _OSTYPE="linux"
 fi
+if [[ ${_OSTYPE} == "darwin"* ]]; then
+    _OSTYPE="darwin"
+fi
+
 
 download()
 {
-    wget -O build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz https://github.com/ethereum/evmone/releases/download/v${TARGET_VERSION}/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz
+    echo "Downloading artifact for '${_OSTYPE}'"
+    wget -O build/_workspace/evmone/evmone-${TARGET_VERSION}-${_OSTYPE}-x86_64.tar.gz https://github.com/ethereum/evmone/releases/download/v${TARGET_VERSION}/evmone-${TARGET_VERSION}-${_OSTYPE}-x86_64.tar.gz
 }
 
 unpack()
@@ -23,10 +27,10 @@ unpack()
     rm -rf build/_workspace/evmone/include
     rm -rf build/_workspace/evmone/lib
 
-    tar xzvf build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz -C build/_workspace/evmone/
+    tar xzvf build/_workspace/evmone/evmone-${TARGET_VERSION}-${_OSTYPE}-x86_64.tar.gz -C build/_workspace/evmone/
 }
 
 mkdir -p build/_workspace/evmone
-[[ -f build/_workspace/evmone/evmone-${TARGET_VERSION}-linux-x86_64.tar.gz ]] || download
+[[ -f build/_workspace/evmone/evmone-${TARGET_VERSION}-${_OSTYPE}-x86_64.tar.gz ]] || download
 unpack
 

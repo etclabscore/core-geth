44581a0e81277a2ca05578dc5e2d74c2674bd082
Chris Ziogas <ziogaschr@gmail.com>
2022-12-12 18:27:01 +0200

    cmd/geth: cmd/geth: geth-test ipc was using a too-long (>108 char) path by default

--- cmd/geth/consolecmd_cg_test.go
+++ cmd/geth/consolecmd_cg_test.go
@@ -165,7 +165,7 @@ func TestGethStartupLogs(t *testing.T) {
 	}
 	for i, c := range cases {
 		t.Run(fmt.Sprintf("TestGethStartupLogs/%d: %v", i, c.flags), func(t *testing.T) {
-			geth := runGeth(t, append(c.flags, "--exec", "admin.nodeInfo.name", "console")...)
+			geth := runGeth(t, append(c.flags, "--ipcpath", filepath.Join(os.TempDir(), "geth.ipc"), "--exec", "admin.nodeInfo.name", "console")...)
 			geth.KillTimeout = 10 * time.Second
 			geth.ExpectRegexp("(?ism).*CoreGeth.*")
 			geth.ExpectExit()

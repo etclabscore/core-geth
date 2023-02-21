8affd9c6b40998f3c78b488905ffa52138ee0b23
meows <b5c6@protonmail.com>
2023-02-21 10:36:36 -0800

    miner: undefined: params
    Date: 2023-02-21 10:36:36-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- miner/miner.go
+++ miner/miner.go
@@ -62,7 +62,7 @@ type Config struct {
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
 	GasCeil:  30000000,
-	GasPrice: big.NewInt(params.GWei),
+	GasPrice: big.NewInt(vars.GWei),
 
 	// The default recommit time is chosen as two seconds since
 	// consensus-layer usually will wait a half slot of time(6s)

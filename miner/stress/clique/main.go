c5c6ffcd31952a58003d2e69e233c68a4db9883c
meows <b5c6@protonmail.com>
2023-02-22 11:29:04 -0800

    miner/stress/clique: "github.com/ethereum/go-ethereum/core" imported and not used
    Date: 2023-02-22 11:29:04-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- miner/stress/clique/main.go
+++ miner/stress/clique/main.go
@@ -29,7 +29,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/fdlimit"
-	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"

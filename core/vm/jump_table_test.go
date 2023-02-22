ad5e0772de40e69266a7434fedded91392714a5f
meows <b5c6@protonmail.com>
2023-02-22 09:13:42 -0800

    core/vm: undefined: newMergeInstructionSet
    Date: 2023-02-22 09:13:42-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/vm/jump_table_test.go
+++ core/vm/jump_table_test.go
@@ -17,18 +17,21 @@
 package vm
 
 import (
+	"math/big"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/require"
 )
 
 // TestJumpTableCopy tests that deep copy is necessery to prevent modify shared jump table
 func TestJumpTableCopy(t *testing.T) {
-	tbl := newMergeInstructionSet()
+	zero := uint64(0)
+	tbl := instructionSetForConfig(params.TestChainConfig, true, big.NewInt(0), &zero)
 	require.Equal(t, uint64(0), tbl[SLOAD].constantGas)
 
 	// a deep copy won't modify the shared jump table
-	deepCopy := copyJumpTable(&tbl)
+	deepCopy := copyJumpTable(tbl)
 	deepCopy[SLOAD].constantGas = 100
 	require.Equal(t, uint64(100), deepCopy[SLOAD].constantGas)
 	require.Equal(t, uint64(0), tbl[SLOAD].constantGas)

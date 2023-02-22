75dd6e1fc0b7f7a3dbe04a203c4337640454694c
meows <b5c6@protonmail.com>
2023-02-22 11:24:08 -0800

    core: TestSetupGenesis redeclared
    This version has lots of errors.
    The other one is pretty much error free.
    Going with that one.
    
    Date: 2023-02-22 11:24:08-08:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/genesis_alloc_test.go
+++ core/genesis_alloc_test.go
@@ -17,22 +17,9 @@
 package core
 
 import (
-	"math/big"
-	"reflect"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/params/confp"
-	"github.com/ethereum/go-ethereum/params/types/ctypes"
-	"github.com/ethereum/go-ethereum/params/types/genesisT"
-	"github.com/ethereum/go-ethereum/params/types/goethereum"
-	"github.com/go-test/deep"
 )
 
 func TestDefaultGenesisBlock(t *testing.T) {
@@ -40,136 +27,8 @@ func TestDefaultGenesisBlock(t *testing.T) {
 	if block.Hash() != params.MainnetGenesisHash {
 		t.Errorf("wrong mainnet genesis hash, got %v, want %v", block.Hash(), params.MainnetGenesisHash)
 	}
-	block = GenesisToBlock(params.DefaultRopstenGenesisBlock(), nil)
-	if block.Hash() != params.RopstenGenesisHash {
-		t.Errorf("wrong testnet genesis hash, got %v, want %v", block.Hash(), params.RopstenGenesisHash)
-	}
-}
-
-func TestSetupGenesis(t *testing.T) {
-	var (
-		customghash = common.HexToHash("0x89c99d90b79719238d2645c7642f2c9295246e80775b38cfd162b696817fbd50")
-		customg     = genesisT.Genesis{
-			Config: &goethereum.ChainConfig{HomesteadBlock: big.NewInt(3)},
-			Alloc: genesisT.GenesisAlloc{
-				{1}: {Balance: big.NewInt(1), Storage: map[common.Hash]common.Hash{{1}: {1}}},
-			},
-		}
-		oldcustomg = customg
-	)
-	oldcustomg.Config = &goethereum.ChainConfig{HomesteadBlock: big.NewInt(2)}
-	tests := []struct {
-		name       string
-		fn         func(ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error)
-		wantConfig *goethereum.ChainConfig
-		wantHash   common.Hash
-		wantErr    error
-	}{
-		{
-			name: "genesis without ChainConfig",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				return SetupGenesisBlock(db, new(genesisT.Genesis))
-			},
-			wantErr:    genesisT.ErrGenesisNoConfig,
-			wantConfig: params.AllEthashProtocolChanges,
-		},
-		{
-			name: "no block in DB, genesis == nil",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				return SetupGenesisBlock(db, nil)
-			},
-			wantHash:   params.MainnetGenesisHash,
-			wantConfig: params.MainnetChainConfig,
-		},
-		{
-			name: "mainnet block in DB, genesis == nil",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				MustCommitGenesis(db, params.DefaultGenesisBlock())
-				return SetupGenesisBlock(db, nil)
-			},
-			wantHash:   params.MainnetGenesisHash,
-			wantConfig: params.MainnetChainConfig,
-		},
-		{
-			name: "custom block in DB, genesis == nil",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				MustCommitGenesis(db, &customg)
-				return SetupGenesisBlock(db, nil)
-			},
-			wantHash:   customghash,
-			wantConfig: customg.Config.(*goethereum.ChainConfig),
-		},
-		{
-			name: "custom block in DB, genesis == testnet",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				MustCommitGenesis(db, &customg)
-				return SetupGenesisBlock(db, params.DefaultRopstenGenesisBlock())
-			},
-			wantErr:    &genesisT.GenesisMismatchError{Stored: customghash, New: params.RopstenGenesisHash},
-			wantHash:   params.RopstenGenesisHash,
-			wantConfig: params.RopstenChainConfig,
-		},
-		{
-			name: "compatible config in DB",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				MustCommitGenesis(db, &oldcustomg)
-				return SetupGenesisBlock(db, &customg)
-			},
-			wantHash:   customghash,
-			wantConfig: customg.Config.(*goethereum.ChainConfig),
-		},
-		{
-			name: "incompatible config in DB",
-			fn: func(db ethdb.Database) (ctypes.ChainConfigurator, common.Hash, error) {
-				// Commit the 'old' genesis block with Homestead transition at #2.
-				// Advance to block #4, past the homestead transition block of customg.
-				genesis := MustCommitGenesis(db, &oldcustomg)
-
-				bc, _ := NewBlockChain(db, nil, oldcustomg.Config, ethash.NewFullFaker(), vm.Config{}, nil, nil)
-				defer bc.Stop()
-
-				blocks, _ := GenerateChain(oldcustomg.Config, genesis, ethash.NewFaker(), db, 4, nil)
-				bc.InsertChain(blocks)
-				bc.CurrentBlock()
-				// This should return a compatibility error.
-				return SetupGenesisBlock(db, &customg)
-			},
-			wantHash:   customghash,
-			wantConfig: customg.Config.(*goethereum.ChainConfig),
-			wantErr: &confp.ConfigCompatError{
-				What:         "incompatible fork value: GetEIP2Transition",
-				StoredConfig: func() *uint64 { b := big.NewInt(2).Uint64(); return &b }(),
-				NewConfig:    func() *uint64 { b := big.NewInt(3).Uint64(); return &b }(),
-				RewindTo:     1,
-			},
-		},
-	}
-
-	for _, test := range tests {
-		db := rawdb.NewMemoryDatabase()
-		config, hash, err := test.fn(db)
-		// Check the return values.
-		if !reflect.DeepEqual(err, test.wantErr) {
-			spew := spew.ConfigState{DisablePointerAddresses: true, DisableCapacities: true}
-			t.Errorf("%s: returned error %#v, want %#v", test.name, spew.NewFormatter(err), spew.NewFormatter(test.wantErr))
-		}
-		if !reflect.DeepEqual(config, test.wantConfig) {
-			spew.Config.DisableMethods = true
-			t.Errorf("%s:\nreturned %v\nwant     %v", test.name, spew.Sdump(config), spew.Sdump(test.wantConfig))
-			diffs := deep.Equal(config, test.wantConfig)
-			t.Log(len(diffs), "differences")
-			for _, d := range diffs {
-				t.Log(d)
-			}
-		}
-		if hash != test.wantHash {
-			t.Errorf("%s: returned hash %s, want %s", test.name, hash.Hex(), test.wantHash.Hex())
-		} else if err == nil {
-			// Check database content.
-			stored := rawdb.ReadBlock(db, test.wantHash, 0)
-			if stored.Hash() != test.wantHash {
-				t.Errorf("%s: block in DB has hash %s, want %s", test.name, stored.Hash(), test.wantHash)
-			}
-		}
+	block = GenesisToBlock(params.DefaultSepoliaGenesisBlock(), nil)
+	if block.Hash() != params.SepoliaGenesisHash {
+		t.Errorf("wrong testnet genesis hash, got %v, want %v", block.Hash(), params.SepoliaGenesisHash)
 	}
 }

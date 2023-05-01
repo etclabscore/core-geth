3a4ce69eff29ba8fb67c764cd58f27933983fb6a
meows <b5c6@protonmail.com>
2023-05-01 08:14:21 -0700

    eth/tracers/internal/tracetest: unknown field Debug in struct literal of type vm.Config
    Date: 2023-05-01 08:14:21-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- eth/tracers/internal/tracetest/calltrace_parity_test.go
+++ eth/tracers/internal/tracetest/calltrace_parity_test.go
@@ -87,7 +87,7 @@ func callTracerParityTestRunner(tracerName string, filename string, dirPath stri
 	if err != nil {
 		return fmt.Errorf("failed to create call tracer: %v", err)
 	}
-	evm := vm.NewEVM(context, txContext, statedb, test.Genesis.Config, vm.Config{Debug: true, Tracer: tracer})
+	evm := vm.NewEVM(context, txContext, statedb, test.Genesis.Config, vm.Config{Tracer: tracer})
 
 	msg, err := core.TransactionToMessage(tx, signer, nil)
 	if err != nil {

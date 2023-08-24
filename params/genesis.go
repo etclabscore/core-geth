d0c6387ff447f82042233c2426d48cf453d5bd7c
Chris Ziogas <ziogaschr@gmail.com>
2023-08-24 11:21:37 +0300

    params: remove Rinkeby configs

--- params/genesis.go
+++ params/genesis.go
@@ -54,18 +54,6 @@ func DefaultSepoliaGenesisBlock() *genesisT.Genesis {
 	}
 }
 
-// DefaultRinkebyGenesisBlock returns the Rinkeby network genesis block.
-func DefaultRinkebyGenesisBlock() *genesisT.Genesis {
-	return &genesisT.Genesis{
-		Config:     RinkebyChainConfig,
-		Timestamp:  1492009146,
-		ExtraData:  hexutil.MustDecode("0x52657370656374206d7920617574686f7269746168207e452e436172746d616e42eb768f2244c8811c63729a21a3569731535f067ffc57839b00206d1ad20c69a1981b489f772031b279182d99e65703f0076e4812653aab85fca0f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
-		GasLimit:   4700000,
-		Difficulty: big.NewInt(1),
-		Alloc:      genesisT.DecodePreAlloc(RinkebyAllocData),
-	}
-}
-
 // DefaultGoerliGenesisBlock returns the Görli network genesis block.
 func DefaultGoerliGenesisBlock() *genesisT.Genesis {
 	return &genesisT.Genesis{

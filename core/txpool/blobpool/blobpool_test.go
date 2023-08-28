d1c5bcad2c51e8c79ee41002273de6a992f7d7ec
meows <b5c6@protonmail.com>
2023-08-28 16:35:17 -0600

    core/txpool/blobpool: undefined: params.BlobTxHashVersion
    Date: 2023-08-28 16:35:17-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/txpool/blobpool/blobpool_test.go
+++ core/txpool/blobpool/blobpool_test.go
@@ -42,6 +42,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethdb/memorydb"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-ethereum/params/vars"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/holiman/billy"
 	"github.com/holiman/uint256"
@@ -60,7 +61,7 @@ func blobHash(commit kzg4844.Commitment) common.Hash {
 	hash := hasher.Sum(nil)
 
 	var vhash common.Hash
-	vhash[0] = params.BlobTxHashVersion
+	vhash[0] = vars.BlobTxHashVersion
 	copy(vhash[1:], hash[1:])
 
 	return vhash
@@ -614,8 +615,8 @@ func TestOpenIndex(t *testing.T) {
 		txExecFeeCaps = []uint64{100, 90, 200, 10, 80, 300}
 		txBlobFeeCaps = []uint64{55, 66, 77, 33, 22, 11}
 
-		//basefeeJumps = []float64{39.098, 38.204, 44.983, 19.549, 37.204, 48.426} // log 1.125 (exec fee cap)
-		//blobfeeJumps = []float64{34.023, 35.570, 36.879, 29.686, 26.243, 20.358} // log 1.125 (blob fee cap)
+		// basefeeJumps = []float64{39.098, 38.204, 44.983, 19.549, 37.204, 48.426} // log 1.125 (exec fee cap)
+		// blobfeeJumps = []float64{34.023, 35.570, 36.879, 29.686, 26.243, 20.358} // log 1.125 (blob fee cap)
 
 		evictExecTipCaps  = []uint64{10, 10, 5, 5, 1, 1}
 		evictExecFeeJumps = []float64{39.098, 38.204, 38.204, 19.549, 19.549, 19.549} //  min(log 1.125 (exec fee cap))

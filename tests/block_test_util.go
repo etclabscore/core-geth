d069a7069b41829a25dfc072dfd23e022d0b9c35
meows <b5c6@protonmail.com>
2023-09-15 06:29:31 -0700

    tests: undefined: genesis
    Date: 2023-09-15 06:29:31-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- tests/block_test_util.go
+++ tests/block_test_util.go
@@ -154,7 +154,7 @@ func (t *BlockTest) Run(snapshotter bool, scheme string, tracer vm.EVMLogger) er
 		cache.SnapshotLimit = 1
 		cache.SnapshotWait = true
 	}
-	chain, err := core.NewBlockChain(db, cache, genesis, nil, engine, vm.Config{
+	chain, err := core.NewBlockChain(db, cache, gspec, nil, engine, vm.Config{
 		Tracer: tracer,
 	}, nil, nil)
 	if err != nil {

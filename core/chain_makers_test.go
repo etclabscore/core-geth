d762231e5d7ae1f81a403bacede7fd2b6fdc018f
meows <b5c6@protonmail.com>
2023-09-16 06:31:12 -0700

    core: not enough arguments in call to MustCommitGenesis
    Date: 2023-09-16 06:31:12-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/chain_makers_test.go
+++ core/chain_makers_test.go
@@ -157,7 +157,7 @@ func ExampleGenerateChain() {
 		Config: &goethereum.ChainConfig{HomesteadBlock: new(big.Int)},
 		Alloc:  genesisT.GenesisAlloc{addr1: {Balance: big.NewInt(1000000)}},
 	}
-	genesis := MustCommitGenesis(genDb, trie.NewDatabase(genDb, trie.HashDefaults))
+	genesis := MustCommitGenesis(genDb, trie.NewDatabase(genDb, trie.HashDefaults), gspec)
 
 	// This call generates a chain of 5 blocks. The function runs for
 	// each block and adds different features to gen based on the

602216b8c56cb22bc8b0694d7d9ceb7c4714908b
meows <b5c6@protonmail.com>
2023-10-17 08:18:22 -0600

    les: unused: var auth (no deploy checkpoint contract)
    Date: 2023-10-17 08:18:22-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- les/test_helper.go
+++ les/test_helper.go
@@ -28,7 +28,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
@@ -118,9 +117,6 @@ func prepare(n int, backend *backends.SimulatedBackend) {
 			// Builtin-block
 			//    number: 1
 			//    txs:    2
-
-			// deploy checkpoint contract
-			auth, _ := bind.NewKeyedTransactorWithChainID(bankKey, big.NewInt(1337))
 
 			// bankUser transfers some ether to user1
 			nonce, _ := backend.PendingNonceAt(ctx, bankAddr)

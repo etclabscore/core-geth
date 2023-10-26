0a0f7c57ec162b7178d2050d2f23159bd6476645
Chetan G <quantumtechniker@gmail.com>
2023-10-26 14:04:39 +0530

    Update 2merkle.go
    The code now provides more detailed error messages for common issues, making it easier to diagnose problems during verification.
    It uses Goroutines and a WaitGroup to parallelize the verification process, which can significantly improve performance when verifying large Merkle trees.
    It checks for index bounds and handles them gracefully, preventing out-of-bounds errors.
    The code is structured to provide cleaner error handling and better concurrent processing.

--- beacon/merkle/merkle.go
+++ beacon/merkle/merkle.go
@@ -14,54 +14,80 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package merkle implements proof verifications in binary merkle trees.
 package merkle
 
 import (
 	"crypto/sha256"
 	"errors"
 	"reflect"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-// Value represents either a 32 byte leaf value or hash node in a binary merkle tree/partial proof.
+// Value represents either a 32 byte leaf value or hash node in a binary Merkle tree/partial proof.
 type Value [32]byte
 
-// Values represent a series of merkle tree leaves/nodes.
+// Values represent a series of Merkle tree leaves/nodes.
 type Values []Value
 
 var valueT = reflect.TypeOf(Value{})
 
-// UnmarshalJSON parses a merkle value in hex syntax.
+// UnmarshalJSON parses a Merkle value in hex syntax.
 func (m *Value) UnmarshalJSON(input []byte) error {
 	return hexutil.UnmarshalFixedJSON(valueT, input, m[:])
 }
 
-// VerifyProof verifies a Merkle proof branch for a single value in a
-// binary Merkle tree (index is a generalized tree index).
+// VerifyProof verifies a Merkle proof branch for a single value in a binary Merkle tree (index is a generalized tree index).
 func VerifyProof(root common.Hash, index uint64, branch Values, value Value) error {
+	if len(branch) == 0 {
+		return errors.New("proof branch is empty")
+	}
+	if index == 0 || index > uint64(len(branch)) {
+		return errors.New("index is out of bounds")
+	}
+
 	hasher := sha256.New()
-	for _, sibling := range branch {
-		hasher.Reset()
-		if index&1 == 0 {
-			hasher.Write(value[:])
-			hasher.Write(sibling[:])
-		} else {
-			hasher.Write(sibling[:])
-			hasher.Write(value[:])
-		}
-		hasher.Sum(value[:0])
-		if index >>= 1; index == 0 {
-			return errors.New("branch has extra items")
-		}
+	var wg sync.WaitGroup
+	var errMutex sync.Mutex
+	var verifyErr error
+
+	// Parallelize proof verification for better performance
+	for i, sibling := range branch {
+		wg.Add(1)
+		go func(i int, sibling Value) {
+			defer wg.Done()
+
+			hasher.Reset()
+			if index&(1<<i) == 0 {
+				hasher.Write(value[:])
+				hasher.Write(sibling[:])
+			} else {
+				hasher.Write(sibling[:])
+				hasher.Write(value[:])
+			}
+			hasher.Sum(value[:0])
+
+			// Use a mutex to safely handle errors
+			errMutex.Lock()
+			if index>>i == 1 && common.Hash(value) != root {
+				verifyErr = errors.New("root mismatch")
+			}
+			errMutex.Unlock()
+		}(i, sibling)
 	}
-	if index != 1 {
-		return errors.New("branch is missing items")
+
+	wg.Wait()
+	if verifyErr != nil {
+		return verifyErr
 	}
-	if common.Hash(value) != root {
-		return errors.New("root mismatch")
+
+	if index>>len(branch) != 1 {
+		return errors.New("branch has extra items")
 	}
+
 	return nil
 }
+
+	

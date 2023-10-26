057d2e173beacac25ec9db2e5c25a75770881baf
meows <b5c6@protonmail.com>
2023-10-26 08:48:38 -0600

    tests: debugging: match test T encodings to release/1.13
    Date: 2023-10-26 08:48:38-06:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- tests/block_test_util.go
+++ tests/block_test_util.go
@@ -29,7 +29,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/beacon"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core"
@@ -140,14 +139,8 @@ func (t *BlockTest) Run(snapshotter bool, scheme string, tracer vm.EVMLogger) er
 	if gblock.Root() != t.json.Genesis.StateRoot {
 		return fmt.Errorf("genesis block state root does not match test: computed=%x, test=%x", gblock.Root().Bytes()[:6], t.json.Genesis.StateRoot[:6])
 	}
-	var engine consensus.Engine
-	if t.json.SealEngine == "NoProof" {
-		engine = ethash.NewFaker()
-	} else {
-		engine = ethash.NewShared()
-	}
 	// Wrap the original engine within the beacon-engine
-	engine = beacon.New(engine)
+	engine := beacon.New(ethash.NewFaker())
 
 	cache := &core.CacheConfig{TrieCleanLimit: 0, StateScheme: scheme}
 	if snapshotter {

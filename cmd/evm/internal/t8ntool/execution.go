d3696be748adf553cc31d43350f52d9bfe330f19
meows <b5c6@protonmail.com>
2023-11-29 08:49:03 -0700

    cmd/evm/internal/t8ntool: undefined: params.BlobTxBlobGasPerBlob
    Date: 2023-11-29 08:49:03-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/evm/internal/t8ntool/execution.go
+++ cmd/evm/internal/t8ntool/execution.go
@@ -214,8 +214,8 @@ func (pre *Prestate) Apply(vmConfig vm.Config, chainConfig ctypes.ChainConfigura
 			continue
 		}
 		if tx.Type() == types.BlobTxType {
-			txBlobGas := uint64(params.BlobTxBlobGasPerBlob * len(tx.BlobHashes()))
-			if used, max := blobGasUsed+txBlobGas, uint64(params.MaxBlobGasPerBlock); used > max {
+			txBlobGas := uint64(vars.BlobTxBlobGasPerBlob * len(tx.BlobHashes()))
+			if used, max := blobGasUsed+txBlobGas, uint64(vars.MaxBlobGasPerBlock); used > max {
 				err := fmt.Errorf("blob gas (%d) would exceed maximum allowance %d", used, max)
 				log.Warn("rejected tx", "index", i, "err", err)
 				rejectedTxs = append(rejectedTxs, &rejectedTx{i, err.Error()})

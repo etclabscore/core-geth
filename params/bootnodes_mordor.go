f955fb97f0ec93a0ece4290c88fc8f8ce93c8397
meows <b5c6@protonmail.com>
2024-02-06 12:24:29 -0700

    params: remove meowsbits mordor bootnode
    Date: 2024-02-06 12:24:29-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- params/bootnodes_mordor.go
+++ params/bootnodes_mordor.go
@@ -20,7 +20,6 @@ package params
 // the Ethereum Classic Mordor network.
 // https://github.com/etclabscore/mordor/blob/master/static-nodes.json
 var MordorBootnodes = []string{
-	"enode://5e85df7bc6d529647cf9a417162784a89b7ccf2b8e1570fadb6fdf9fa025c8ec2257825d1ec5d7357a6f49898fdfbd9c4c56d22645dbe8b8a6aa67dacbcf3ecc@157.230.152.87:30303", // meowsbits@sfetclabs
 	"enode://4539a067ae1f6a7ffac509603ba37baf772fc832880ddc67c53f292b6199fb048267f0311c820bc90bfd39ec663bc6b5256bdf787ec38425c82bde6bc2bcfe3c@24.199.107.164:30303", // @etccoop-sfo
 }
 var MordorDNSNetwork1 = dnsPrefixETC + "all.mordor.blockd.info"

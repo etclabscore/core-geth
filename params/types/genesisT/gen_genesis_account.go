ff7eaf02fa10f726ef64ff145448d11eb094c80b
meows <b5c6@protonmail.com>
2024-02-22 12:21:02 -0700

    params/types/genesisT: go run github.com/fjl/gencodec -type GenesisAccount -field-override genesisAccountMarshaling -out gen_genesis_account.go
    Date: 2024-02-22 12:21:02-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- params/types/genesisT/gen_genesis_account.go
+++ params/types/genesisT/gen_genesis_account.go
@@ -12,62 +12,62 @@ import (
 	"github.com/ethereum/go-ethereum/common/math"
 )
 
-var _ = (*accountMarshaling)(nil)
+var _ = (*genesisAccountMarshaling)(nil)
 
 // MarshalJSON marshals as JSON.
-func (a Account) MarshalJSON() ([]byte, error) {
-	type Account struct {
+func (g GenesisAccount) MarshalJSON() ([]byte, error) {
+	type GenesisAccount struct {
 		Code       hexutil.Bytes               `json:"code,omitempty"`
 		Storage    map[storageJSON]storageJSON `json:"storage,omitempty"`
 		Balance    *math.HexOrDecimal256       `json:"balance" gencodec:"required"`
 		Nonce      math.HexOrDecimal64         `json:"nonce,omitempty"`
 		PrivateKey hexutil.Bytes               `json:"secretKey,omitempty"`
 	}
-	var enc Account
-	enc.Code = a.Code
-	if a.Storage != nil {
-		enc.Storage = make(map[storageJSON]storageJSON, len(a.Storage))
-		for k, v := range a.Storage {
+	var enc GenesisAccount
+	enc.Code = g.Code
+	if g.Storage != nil {
+		enc.Storage = make(map[storageJSON]storageJSON, len(g.Storage))
+		for k, v := range g.Storage {
 			enc.Storage[storageJSON(k)] = storageJSON(v)
 		}
 	}
-	enc.Balance = (*math.HexOrDecimal256)(a.Balance)
-	enc.Nonce = math.HexOrDecimal64(a.Nonce)
-	enc.PrivateKey = a.PrivateKey
+	enc.Balance = (*math.HexOrDecimal256)(g.Balance)
+	enc.Nonce = math.HexOrDecimal64(g.Nonce)
+	enc.PrivateKey = g.PrivateKey
 	return json.Marshal(&enc)
 }
 
 // UnmarshalJSON unmarshals from JSON.
-func (a *Account) UnmarshalJSON(input []byte) error {
-	type Account struct {
+func (g *GenesisAccount) UnmarshalJSON(input []byte) error {
+	type GenesisAccount struct {
 		Code       *hexutil.Bytes              `json:"code,omitempty"`
 		Storage    map[storageJSON]storageJSON `json:"storage,omitempty"`
 		Balance    *math.HexOrDecimal256       `json:"balance" gencodec:"required"`
 		Nonce      *math.HexOrDecimal64        `json:"nonce,omitempty"`
 		PrivateKey *hexutil.Bytes              `json:"secretKey,omitempty"`
 	}
-	var dec Account
+	var dec GenesisAccount
 	if err := json.Unmarshal(input, &dec); err != nil {
 		return err
 	}
 	if dec.Code != nil {
-		a.Code = *dec.Code
+		g.Code = *dec.Code
 	}
 	if dec.Storage != nil {
-		a.Storage = make(map[common.Hash]common.Hash, len(dec.Storage))
+		g.Storage = make(map[common.Hash]common.Hash, len(dec.Storage))
 		for k, v := range dec.Storage {
-			a.Storage[common.Hash(k)] = common.Hash(v)
+			g.Storage[common.Hash(k)] = common.Hash(v)
 		}
 	}
 	if dec.Balance == nil {
-		return errors.New("missing required field 'balance' for Account")
+		return errors.New("missing required field 'balance' for GenesisAccount")
 	}
-	a.Balance = (*big.Int)(dec.Balance)
+	g.Balance = (*big.Int)(dec.Balance)
 	if dec.Nonce != nil {
-		a.Nonce = uint64(*dec.Nonce)
+		g.Nonce = uint64(*dec.Nonce)
 	}
 	if dec.PrivateKey != nil {
-		a.PrivateKey = *dec.PrivateKey
+		g.PrivateKey = *dec.PrivateKey
 	}
 	return nil
 }

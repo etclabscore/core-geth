a0bcff75093932c3bb6b799ca4849729fa57bd16
meows <b5c6@protonmail.com>
2024-02-27 09:13:20 -0700

    cmd/utils: unresolve CommitGenesis
    Date: 2024-02-27 09:13:20-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/utils/history_test.go
+++ cmd/utils/history_test.go
@@ -172,7 +172,7 @@ func TestHistoryImportAndExport(t *testing.T) {
 		db2.Close()
 	})
 
-	genesis.MustCommit(db2, triedb.NewDatabase(db, triedb.HashDefaults))
+	core.MustCommitGenesis(db2, triedb.NewDatabase(db, triedb.HashDefaults), genesis)
 	imported, err := core.NewBlockChain(db2, nil, genesis, nil, ethash.NewFaker(), vm.Config{}, nil, nil)
 	if err != nil {
 		t.Fatalf("unable to initialize chain: %v", err)

fd9327d30d1b9ef5d9faa0a914fc2a9a958c7f71
dashangcun <907225865@qq.com>
2025-01-09 22:26:13 +0800

    chore: make function comments match function names
    Signed-off-by: dashangcun <907225865@qq.com>

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -491,7 +491,7 @@ func makeBlockChain(chainConfig ctypes.ChainConfigurator, parent *types.Block, n
 	return blocks
 }
 
-// makeBlockChain creates a deterministic chain of blocks from genesis
+// makeBlockChainWithGenesis creates a deterministic chain of blocks from genesis
 func makeBlockChainWithGenesis(genesis *genesisT.Genesis, n int, engine consensus.Engine, seed int) (ethdb.Database, []*types.Block) {
 	db, blocks, _ := GenerateChainWithGenesis(genesis, engine, n, func(i int, b *BlockGen) {
 		b.SetCoinbase(common.Address{0: byte(seed), 19: byte(i)})
